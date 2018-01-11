bc80830bc0e52aff1fc0dbc6d396d26084d427fb
Brian Thomas Kennedy <briankennedy@google.com>
2018-01-10 18:00:23 -0800

    Remove deprecated script.
    Change-Id: Ibf0e9f91c98888ffffb5de7ac4a3b2c130ad4a1a

--- scripts/cluster/gce/download-k8s-1.8.3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-rm -rf _kubernetes
-wget https://github.com/kubernetes/kubernetes/releases/download/v1.8.3/kubernetes.tar.gz -P /tmp
-tar -xf /tmp/kubernetes.tar.gz
-mv kubernetes _kubernetes
-rm -rf /tmp/kubernetes.tar.gz
