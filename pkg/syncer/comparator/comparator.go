079870865bb78aab2a8239dc97b9bec9d9acb27b
Brian Thomas Kennedy <briankennedy@google.com>
2018-03-30 16:49:31 -0700

    Implement comparator for arbitrary resource sets.
    Bug: 77530151
    Change-Id: I053bdfbcbbe071bacaac44f9637cba54d6cc779d

---
+++ pkg/syncer/comparator/comparator.go
@@ -0,0 +1,118 @@
+/*
+Copyright 2018 The Nomos Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package comparator
+
+import (
+	"github.com/google/nomos/pkg/client/action"
+
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Type indicates the state of the given resource
+type Type string
+
+const (
+	// Add indicates the resource is declared, but missing on the API server
+	Add = Type("add")
+	// Update indicates the resource is declared, but different on the API server
+	Update = Type("update")
+	// Delete indicates the resource is not declared, but exists on the API server
+	Delete = Type("delete")
+)
+
+// Equals is a function that takes two objects then compares them while ignoring the object meta
+// labels and annotations.
+type Equals func(meta_v1.Object, meta_v1.Object) bool
+
+// Diff is resource where Declared and Actual do not match.
+type Diff struct {
+	Name     string
+	Type     Type
+	Declared meta_v1.Object
+	Actual   meta_v1.Object
+}
+
+// Comparitor handles comparing declared to actual on arbitrary resources given a type-specific
+// equlivalence function.
+type Comparitor struct {
+	equals Equals
+}
+
+// New returns a new reconciler which will use the given equals function
+func New(equals Equals) *Comparitor {
+	return &Comparitor{equals: equals}
+}
+
+// Compare returns the diffs between declared and actual state. The diffs will be returned in an
+// arbitrary order.
+func (s *Comparitor) Compare(declared []meta_v1.Object, actual []meta_v1.Object) []*Diff {
+	return Compare(s.equals, declared, actual)
+}
+
+// Compare returns the diffs between declared and actual state. The diffs will be returned in an
+// arbitrary order.
+func Compare(equals Equals, declared []meta_v1.Object, existing []meta_v1.Object) []*Diff {
+	var diffs []*Diff
+
+	decls := map[string]meta_v1.Object{}
+	for _, decl := range declared {
+		decls[decl.GetName()] = decl
+	}
+
+	actuals := map[string]meta_v1.Object{}
+	for _, item := range existing {
+		actuals[item.GetName()] = item
+	}
+
+	for name, decl := range decls {
+		if actual, found := actuals[name]; !found {
+			// in decl, not in actual
+			diffs = append(diffs, &Diff{
+				Name:     name,
+				Type:     Add,
+				Declared: decl,
+				Actual:   nil,
+			})
+		} else {
+			// We check equals on content body, but meta subset on the labels / annotations because
+			// it's possible for another controller to add some form of annotation or label to the object
+			// without changing the intent of what's going on. For an example of this, see ClusterRole
+			// aggregation.
+			if !equals(decl, actual) || !action.MetaSubset(actual, decl) {
+				diffs = append(diffs, &Diff{
+					Name:     name,
+					Type:     Update,
+					Declared: decl,
+					Actual:   actual,
+				})
+			}
+		}
+	}
+
+	for name, actual := range actuals {
+		if _, found := decls[name]; !found {
+			// Not in declared, in actuals
+			diffs = append(diffs, &Diff{
+				Name:     name,
+				Type:     Delete,
+				Declared: nil,
+				Actual:   actual,
+			})
+		}
+	}
+	return diffs
+}
---
+++ pkg/syncer/comparator/comparator_test.go
@@ -0,0 +1,197 @@
+/*
+Copyright 2018 The Nomos Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package comparator
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type TestObject struct {
+	meta_v1.ObjectMeta
+	meta_v1.TypeMeta
+
+	value string
+}
+
+func testEquals(lhsObj meta_v1.Object, rhsObj meta_v1.Object) bool {
+	lhs := lhsObj.(*TestObject)
+	rhs := rhsObj.(*TestObject)
+	return lhs.value == rhs.value
+}
+
+type TestItem struct {
+	name        string
+	value       string
+	labels      map[string]string
+	annotations map[string]string
+}
+
+func (t TestItem) Object() meta_v1.Object {
+	return &TestObject{
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name:        t.name,
+			Labels:      t.labels,
+			Annotations: t.annotations,
+		},
+		value: t.value,
+	}
+}
+
+type TestItems []TestItem
+
+func (t TestItems) Objects() []meta_v1.Object {
+	ret := make([]meta_v1.Object, len(t))
+	for idx, item := range t {
+		ret[idx] = item.Object()
+	}
+	return ret
+}
+
+type TestCase struct {
+	name    string
+	decls   TestItems
+	actuals TestItems
+	expect  []*Diff
+}
+
+func toMap(diffs []*Diff) map[string]*Diff {
+	ret := map[string]*Diff{}
+	for _, diff := range diffs {
+		if diff.Actual != nil {
+			ret[diff.Actual.GetName()] = diff
+		} else {
+			ret[diff.Declared.GetName()] = diff
+		}
+	}
+	return ret
+}
+
+func TestComparitor(t *testing.T) {
+	testcases := []TestCase{
+		TestCase{
+			name: "Empty sets",
+		},
+		TestCase{
+			name: "Only declared",
+			decls: TestItems{
+				TestItem{name: "foo", value: "1"},
+			},
+			expect: []*Diff{
+				&Diff{
+					Name:     "foo",
+					Type:     Add,
+					Declared: TestItem{name: "foo", value: "1"}.Object(),
+					Actual:   nil,
+				},
+			},
+		},
+		TestCase{
+			name: "Only actual",
+			actuals: TestItems{
+				TestItem{name: "foo", value: "2"},
+			},
+			expect: []*Diff{
+				&Diff{
+					Name:     "foo",
+					Type:     Delete,
+					Declared: nil,
+					Actual:   TestItem{name: "foo", value: "2"}.Object(),
+				},
+			},
+		},
+		TestCase{
+			name: "Differing element value",
+			decls: TestItems{
+				TestItem{name: "foo", value: "1"},
+			},
+			actuals: TestItems{
+				TestItem{name: "foo", value: "2"},
+			},
+			expect: []*Diff{
+				&Diff{
+					Name:     "foo",
+					Type:     Update,
+					Declared: TestItem{name: "foo", value: "1"}.Object(),
+					Actual:   TestItem{name: "foo", value: "2"}.Object(),
+				},
+			},
+		},
+		TestCase{
+			name: "Mixture",
+			decls: TestItems{
+				TestItem{name: "foo", value: "1"},
+				TestItem{name: "bar", value: "2"},
+				TestItem{name: "baz", value: "3"},
+			},
+			actuals: TestItems{
+				TestItem{name: "foo", value: "2"},
+				TestItem{name: "bar", value: "2"},
+				TestItem{name: "buffalo", value: "4"},
+			},
+			expect: []*Diff{
+				&Diff{
+					Name:     "foo",
+					Type:     Update,
+					Declared: TestItem{name: "foo", value: "1"}.Object(),
+					Actual:   TestItem{name: "foo", value: "2"}.Object(),
+				},
+				&Diff{
+					Name:     "baz",
+					Type:     Add,
+					Declared: TestItem{name: "baz", value: "3"}.Object(),
+					Actual:   nil,
+				},
+				&Diff{
+					Name:     "buffalo",
+					Type:     Delete,
+					Declared: nil,
+					Actual:   TestItem{name: "buffalo", value: "4"}.Object(),
+				},
+			},
+		},
+	}
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			declared := testcase.decls.Objects()
+			actuals := testcase.actuals.Objects()
+
+			cmp := New(testEquals)
+			diff := cmp.Compare(declared, actuals)
+
+			for _, expect := range testcase.expect {
+				var found bool
+				for _, actual := range diff {
+					if reflect.DeepEqual(expect, actual) {
+						found = true
+					}
+				}
+				if !found {
+					t.Errorf("expected diff %#v missing from actual", spew.Sdump(expect))
+					fmt.Printf("expect:\n%s\ndiff:\n%s\n", spew.Sdump(expect), spew.Sdump(diff))
+				}
+			}
+			if len(diff) != len(testcase.expect) {
+				t.Errorf("Expected was different length than actual")
+			}
+		})
+	}
+}
