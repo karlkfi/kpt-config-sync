9df4f673813be879706dc85c4fb2a325371cbb02
Brian Thomas Kennedy <briankennedy@google.com>
2018-04-09 14:45:04 -0700

    Add a package for dealing with Object types.
    Change-Id: I1e7e319d0e564f80776f757cffa6599c4ae78ab0

---
+++ pkg/client/object/conversion.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2018 The Nomos Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package object exists to make converting between the meta v1 Object interface and the
+// runtime.Object interface less painful.  Both generally refer to API types, but for some reason
+// they are separate interfaces and various libraries like using one or the other, so this exists
+// to help with converting between the two without duplicating the same for loop a bunch of times.
+package object
+
+import (
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// RuntimeToMeta converts a list of runtime.Object to a list of meta_v1.Object
+func RuntimeToMeta(runtimeObjs []runtime.Object) []meta_v1.Object {
+	metaObjs := make([]meta_v1.Object, len(runtimeObjs))
+	for idx := range runtimeObjs {
+		metaObjs[idx] = runtimeObjs[idx].(meta_v1.Object)
+	}
+	return metaObjs
+}
+
+// RuntimeToMetaMap converts a map of string to runtime.Object to a map of string to meta_v1.Object
+func RuntimeToMetaMap(runMap map[string]runtime.Object) map[string]meta_v1.Object {
+	metaMap := map[string]meta_v1.Object{}
+	for k, v := range runMap {
+		metaMap[k] = v.(meta_v1.Object)
+	}
+	return metaMap
+}
+
+// MetaToRuntime converts a list of meta_v1.Object to a list of runtime.Object
+func MetaToRuntime(metaObjs []meta_v1.Object) []runtime.Object {
+	runtimeObjs := make([]runtime.Object, len(metaObjs))
+	for idx := range runtimeObjs {
+		runtimeObjs[idx] = metaObjs[idx].(runtime.Object)
+	}
+	return runtimeObjs
+}
+
+// MetaToRuntimeMap converts a map of string to meta_v1.Object to a map of string to runtime.Object
+func MetaToRuntimeMap(metaMap map[string]meta_v1.Object) map[string]runtime.Object {
+	runMap := map[string]runtime.Object{}
+	for k, v := range metaMap {
+		runMap[k] = v.(runtime.Object)
+	}
+	return runMap
+}
