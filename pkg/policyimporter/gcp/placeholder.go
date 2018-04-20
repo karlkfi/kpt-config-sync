efca6ab022622e6aed52dceca77999c248f9d1bf
Frank Farzan <frankf@google.com>
2018-04-19 17:47:47 -0700

    Placeholder to make grpc deps used.
    Change-Id: I9d9fd2ebd5c523e8bf1ee3fa395cc1ce0356bec4

---
+++ pkg/policyimporter/gcp/placeholder.go
@@ -0,0 +1,12 @@
+package gcp
+
+import (
+	"google.golang.org/genproto/googleapis/watcher/v1"
+	"google.golang.org/grpc"
+)
+
+// nolint: deadcode
+func placeholder() {
+	_ = watcher.Change_DOES_NOT_EXIST
+	_ = grpc.Address{}
+}
