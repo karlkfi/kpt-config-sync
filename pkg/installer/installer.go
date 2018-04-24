a7a8f53d44d4bbe003ab086c26c0047d60601a28
Stephen Bochinski <sbochins@google.com>
2018-04-24 15:32:14 -0700

    Fix GIT_SYNC_SSH config to not always be wrong.
    Bug: 78531016
    Change-Id: Ie2a2ddc61dbc89be0b1ff1e09d2f51de82504b5d

--- pkg/installer/installer.go
+++ pkg/installer/installer.go
@@ -175,7 +175,7 @@ func (i *Installer) deployGitConfig() error {
 
 	if err := c.CreateConfigmapFromLiterals(
 		configmap, defaultNamespace,
-		fmt.Sprintf("GIT_SYNC_SSH=%v", i.c.Ssh == nil),
+		fmt.Sprintf("GIT_SYNC_SSH=%v", i.c.Ssh != nil),
 		fmt.Sprintf("GIT_SYNC_REPO=%v", i.c.Git.SyncRepo),
 		fmt.Sprintf("GIT_SYNC_BRANCH=%v", i.c.Git.SyncBranch),
 		fmt.Sprintf("GIT_SYNC_WAIT=%v", i.c.Git.SyncWaitSeconds),
