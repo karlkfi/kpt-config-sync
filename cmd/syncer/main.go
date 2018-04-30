2ea3ddb1780af5640f3ab46e5fa2649ca76bef45
Brian Thomas Kennedy <briankennedy@google.com>
2018-04-30 15:33:11 -0700

    Syncer: The syncer is dead, long live the syncer.
    Remove old syncer code.
    
    Change-Id: I0b4a8dc30f79a8d5dc8acdefb3c485a7be6a7315

--- cmd/syncer/main.go
+++ cmd/syncer/main.go
@@ -19,22 +19,14 @@ package main
 import (
 	"flag"
 
-	"github.com/golang/glog"
-	"github.com/google/nomos/pkg/client/meta"
 	"github.com/google/nomos/pkg/client/restconfig"
 	"github.com/google/nomos/pkg/service"
-	"github.com/google/nomos/pkg/syncer"
 	"github.com/google/nomos/pkg/syncer/args"
 	"github.com/google/nomos/pkg/syncer/syncercontroller"
 	"github.com/google/nomos/pkg/util/log"
 	"github.com/kubernetes-sigs/kubebuilder/pkg/inject/run"
 	"github.com/kubernetes-sigs/kubebuilder/pkg/signals"
 	"github.com/pkg/errors"
-	"k8s.io/client-go/rest"
-)
-
-var (
-	useNewSyncer = flag.Bool("useNewSyncer", false, "use the new syncer")
 )
 
 func main() {
@@ -48,35 +40,6 @@ func main() {
 
 	go service.ServeMetrics()
 
-	if *useNewSyncer {
-		newSyncerMain(restConfig)
-	} else {
-		syncerMain(restConfig)
-	}
-}
-
-func syncerMain(restConfig *rest.Config) {
-	client, err := meta.NewForConfig(restConfig)
-	if err != nil {
-		panic(errors.Wrapf(err, "Failed to create client"))
-	}
-
-	stopChannel := make(chan struct{})
-	errorCallback := func(err error) {
-		glog.Errorf("Got error from error callback: %s", err)
-		close(stopChannel)
-	}
-
-	clusterSyncer := syncer.New(client)
-	clusterSyncer.Run(errorCallback)
-
-	service.WaitForShutdownWithChannel(stopChannel)
-
-	clusterSyncer.Stop()
-	clusterSyncer.Wait()
-}
-
-func newSyncerMain(restConfig *rest.Config) {
 	injectArgs := args.CreateInjectArgs(restConfig)
 	syncerController := syncercontroller.New(injectArgs)
 
