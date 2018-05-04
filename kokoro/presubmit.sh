fe55a5b659c01fc4578972b4d437a99f901ead87
Frank Farzan <frankf@google.com>
2018-05-03 19:39:14 -0700

    Kokoro: No need to do "go get" or "git clone".
    All our dependencies are vendored and our build is dockerized.
    
    Change-Id: Iddf777860e6976284ef356e67c8c5e777e230305

--- kokoro/presubmit.sh
+++ kokoro/presubmit.sh
@@ -2,24 +2,7 @@
 
 # Fail on any error.
 set -e
-
-echo "============= Setting up NOMOS environment ========"
-export GOPATH=/tmpfs/src/git/go
-NOMOS_DIR=$GOPATH/src/github.com/google/nomos
-mkdir -p $NOMOS_DIR
-
-# Copy our code over to github.com/nomos because that's the import path
-git clone git/stolos $NOMOS_DIR
-
-cd $NOMOS_DIR
-
-# Go get dependencies: Don't install, include test
-echo "Go get ..."
-go get -d -t ../...
-
-echo "======================== BUILD ====================="
-make build
-
-echo "======================== TEST ======================"
+cd /tmpfs/src/git/stolos
+cat kokoro/banner.txt
 make test
 
