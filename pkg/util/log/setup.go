1eeca15f0e41b53b51dea5b3c1533b1082c5bb14
Frank Farzan <frankf@google.com>
2018-05-03 18:49:22 -0700

    Log build version of Nomos binaries on start up.
    This makes debugging easier.
    
    Change-Id: Ic34d956d2630c409b975b3a7156a5b5c58247169

--- pkg/util/log/setup.go
+++ pkg/util/log/setup.go
@@ -1,8 +1,13 @@
 package log
 
-import "flag"
+import (
+	"flag"
 
-// Setup sets up default logging parameters for nomos controllers
+	"github.com/golang/glog"
+	"github.com/google/nomos/pkg/version"
+)
+
+// Setup sets up default logging configs for Nomos applications and logs the preamble.
 func Setup() {
 	if err := flag.Set("logtostderr", "true"); err != nil {
 		panic(err)
@@ -10,4 +15,5 @@ func Setup() {
 	if err := flag.Set("v", "2"); err != nil {
 		panic(err)
 	}
+	glog.Infof("Build Version: %s", version.VERSION)
 }
