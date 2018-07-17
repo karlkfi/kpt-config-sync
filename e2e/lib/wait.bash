3934d5758e5bb68118558dda6acf06678d3f1769
Brian Thomas Kennedy <briankennedy@google.com>
2018-07-16 18:07:46 -0700

    Add new wait function.
    Future change will replace calls to wait::for with the new impl
    
    Change-Id: I4918b3d812f344f9874c0eb949df95ebac8aad79

--- e2e/lib/wait.bash
+++ e2e/lib/wait.bash
@@ -67,14 +67,14 @@ function wait::event() {
 
 function wait::for_success() {
   local command="${1:-}"
-  local timeout=${2:-10}
+  local timeout=${2:-20}
   local or_die=${3:-true}
   wait::for "${command}" "${timeout}" "${or_die}" true
 }
 
 function wait::for_failure() {
   local command="${1:-}"
-  local timeout=${2:-10}
+  local timeout=${2:-20}
   local or_die=${3:-true}
   wait::for "${command}" "${timeout}" "${or_die}" false
 }
@@ -91,33 +91,114 @@ function wait::for_failure() {
 #
 function wait::for() {
   local command="${1:-}"
-  local timeout=${2:-10}
-  local or_die=${3:-true}
-  local expect=${4:-true}
+  local timeout="${2:-10}"
+  local or_die="${3:-true}"
+  local expect="${4:-true}"
 
-  echo -n "Waiting for ${command} to exit ${expect}"
-  # shellcheck disable=SC2034
-  for i in $(seq 1 "${timeout}"); do
-    # shellcheck disable=SC2086
-    run ${command} &> /dev/null
-    if [ "$status" -eq 0 ]; then
-      if ${expect}; then
-        echo
-        return 0
-      fi
-    else
-      if ! ${expect}; then
-        echo
-        return 0
-      fi
-    fi
-    echo -n "."
-    sleep 0.5
-  done
-  echo
-  echo "Command '${command}' failed after ${timeout} seconds"
-  if ${or_die}; then
-    exit 1
+  args=(wait::__for -t "${timeout}")
+  if "${expect}"; then
+    args+=(-s)
+  else
+    args+=(-f)
   fi
+  args+=(--)
+  # shellcheck disable=SC2206
+  args+=(${command})
+
+  if ! "${args[@]}"; then
+    if ${or_die}; then
+      exit 1
+    fi
+  fi
+}
+
+# A newer version of wait::for that does a better job at dealing with args
+# Flags
+#   -s                 Wait for success (exit 0)
+#   -f                 Wait for failure (exit nonzero)
+#   -e [exit code]     Wait for specific integer exit code
+#   -t [timeout]       The timeout in seconds
+#   -d [deadline]      The deadline in seconds since the epoch
+#   -p [poll interval] The amount of time to wait between executions
+#   -- End of flags, command starts after this
+# Args
+#  Args for command
+function wait::__for() {
+  local args=()
+  local deadline=""
+  local sleeptime="0.1"
+  local exitf=(wait::__exit_eq 0)
+
+  local parse_args=false
+  for i in "$@"; do
+    if [[ "$i" == "--" ]]; then
+      parse_args=true
+    fi
+  done
+
+  if $parse_args; then
+    while [[ $# -gt 0 ]]; do
+      local arg="${1:-}"
+      shift
+      case $arg in
+        -s)
+          exitf=(wait::__exit_eq 0)
+        ;;
+        -f)
+          exitf=(wait::__exit_neq 0)
+        ;;
+        -e)
+          exitf=(wait::__exit_eq "${1:-}")
+          shift
+        ;;
+        -t)
+          deadline=$(( $(date +%s) + timeout ))
+          shift
+        ;;
+        -d)
+          deadline=${1:-}
+          shift
+        ;;
+        -p)
+          sleeptime=${1:-}
+          shift
+        ;;
+        --)
+          break
+        ;;
+        *)
+          echo "Unexpected arg $arg" >&3
+          return 1
+        ;;
+      esac
+    done
+  fi
+  args=("$@")
+
+  local out
+  local status
+  while (( "$(date +%s)" < "${deadline}" )); do
+    status=0
+    out="$("${args[@]}")" || status=$?
+    if "${exitf[@]}" "${status}"; then
+      return 0
+    fi
+    sleep "$sleeptime"
+  done
+
+  echo "Command timed out:" "${args[@]}" "last output: ${out}"
+  return 1
+}
+
+function wait::__exit_eq() {
+  local lhs="${1:-}"
+  local rhs="${2:-}"
+  (( lhs == rhs ))
+}
+
+function wait::__exit_neq() {
+  local lhs="${1:-}"
+  local rhs="${2:-}"
+  (( lhs != rhs ))
 }
 
