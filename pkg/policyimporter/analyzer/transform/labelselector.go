5bee185e761358cbf474ca61e26de3a07436f2be
Frank Farzan <frankf@google.com>
2018-08-31 14:01:51 -0700

    LBP: Add utility for evaluating applicability of a policy to a namespace.
    Bug: 112661778
    Change-Id: I49815d39df3326ba25261b712b4a4fc20aa311eb

---
+++ pkg/policyimporter/analyzer/transform/labelselector.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2018 The Nomos Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package transform
+
+import (
+	"encoding/json"
+	"fmt"
+
+	policyhierarchy_v1 "github.com/google/nomos/pkg/api/policyhierarchy/v1"
+	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	labelpkg "k8s.io/apimachinery/pkg/labels"
+)
+
+func isSelected(labels map[string]string, labelselector *metav1.LabelSelector) (bool, error) {
+	selector, err := metav1.LabelSelectorAsSelector(labelselector)
+	if err != nil {
+		return false, fmt.Errorf("invalid label selector: %v", err)
+	}
+	if selector.Empty() {
+		return false, fmt.Errorf("empty label selector")
+	}
+
+	return selector.Matches(labelpkg.Set(labels)), nil
+}
+
+// IsPolicyApplicableToNamespace returns whether the NamespaceSelector annotation on the given policy object
+// matches the given labels on a namespace.
+// The policy is applicable if it has no such annotation.
+func IsPolicyApplicableToNamespace(nsLabels map[string]string, policy metav1.Object) bool {
+	ls, exists := policy.GetAnnotations()[policyhierarchy_v1.NamespaceSelectorAnnotationKey]
+	if !exists {
+		return true
+	}
+	var ns policyhierarchy_v1.NamespaceSelector
+	if err := json.Unmarshal([]byte(ls), &ns); err != nil {
+		panic(errors.Wrapf(err, "failed to unmarshal NamespaceSelector in object %q", policy.GetName()))
+	}
+	selected, err := isSelected(nsLabels, &ns.Spec.Selector)
+	if err != nil {
+		panic(errors.Wrapf(err, "failed to evaluate LabelSelector in object %q", policy.GetName()))
+	}
+	return selected
+}
---
+++ pkg/policyimporter/analyzer/transform/labelselector_test.go
@@ -0,0 +1,108 @@
+/*
+Copyright 2018 The Nomos Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package transform
+
+import (
+	"testing"
+
+	"encoding/json"
+
+	v1 "github.com/google/nomos/pkg/api/policyhierarchy/v1"
+	rbac_v1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var simpleSelector = v1.NamespaceSelector{
+	Spec: v1.NamespaceSelectorSpec{
+		Selector: metav1.LabelSelector{
+			MatchLabels: map[string]string{"env": "prod"}}}}
+
+var complexSelector = v1.NamespaceSelector{
+	Spec: v1.NamespaceSelectorSpec{
+		Selector: metav1.LabelSelector{
+			MatchExpressions: []metav1.LabelSelectorRequirement{{Key: "privacy", Operator: metav1.LabelSelectorOpIn, Values: []string{"sensitive", "restricted"}}},
+			MatchLabels:      map[string]string{"env": "prod"}}}}
+
+type testCase struct {
+	testName           string
+	policy             metav1.Object
+	nsLabels           map[string]string
+	expectedApplicable bool
+}
+
+func TestIsPolicyApplicableToNamespace(t *testing.T) {
+	testCases := []testCase{
+		{
+			testName:           "No annotation",
+			policy:             createPolicy(nil),
+			nsLabels:           map[string]string{"env": "prod"},
+			expectedApplicable: true,
+		},
+		{
+			testName:           "Simple selector",
+			policy:             createPolicy(&simpleSelector),
+			nsLabels:           map[string]string{"env": "prod"},
+			expectedApplicable: true,
+		},
+		{
+			testName:           "Complex selector",
+			policy:             createPolicy(&complexSelector),
+			nsLabels:           map[string]string{"env": "prod", "privacy": "sensitive"},
+			expectedApplicable: true,
+		},
+		{
+			testName:           "No match",
+			policy:             createPolicy(&complexSelector),
+			nsLabels:           map[string]string{"env": "prod", "privacy": "open"},
+			expectedApplicable: false,
+		},
+		{
+			testName:           "No labels",
+			policy:             createPolicy(&simpleSelector),
+			expectedApplicable: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+
+			applicable := IsPolicyApplicableToNamespace(tc.nsLabels, tc.policy)
+			if tc.expectedApplicable != applicable {
+				t.Fatalf("Result didn't match, expected=%t, actual=%t", tc.expectedApplicable, applicable)
+			}
+
+		})
+	}
+}
+
+func createPolicy(s *v1.NamespaceSelector) metav1.Object {
+	rb := &rbac_v1.RoleBinding{}
+	rb.SetName("rb")
+	if s != nil {
+		j, err := json.Marshal(s)
+		if err != nil {
+			panic(err)
+		}
+		rb.SetAnnotations(map[string]string{v1.NamespaceSelectorAnnotationKey: string(j)})
+	}
+	o, err := meta.Accessor(rb)
+	if err != nil {
+		panic(err)
+	}
+	return o
+}
