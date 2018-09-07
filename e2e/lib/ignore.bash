97f9ecd0b3f494fca0467f1ec6044748e4daea95
Erik Kitson <ekitson@google.com>
2018-09-07 11:43:06 -0700

    Fix ignore.bash by ignoring lint-bash's terrible advice.
    Bug:114047010
    Change-Id: I4b14683141c2a695dea00adc2f93a4bf30d944c4

--- e2e/lib/ignore.bash
+++ e2e/lib/ignore.bash
@@ -8,13 +8,13 @@
 # error. Error output will be logged in the event of error.
 #
 ignore::log_err() {
-  local out
-  out="$("$@" 2>&1)"
-  if ! $?
-  then
-  debug::log "$@"
-  debug::log "resulted in error:"
-  debug::log "${out}"
+  # shellcheck disable=SC2155
+  local out="$("$@" 2>&1)"
+  # shellcheck disable=SC2181
+  if [ $? -ne 0 ]; then
+    debug::log "$@"
+    debug::log "resulted in error:"
+    debug::log "${out}"
   fi
   return 0
 }
