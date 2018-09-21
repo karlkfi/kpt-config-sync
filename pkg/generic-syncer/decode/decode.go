90250c586e6ee5ea29accc1c41ee5045f3afa7c4
Stephen Bochinski <sbochins@google.com>
2018-09-21 13:55:32 -0700

    decode generic resources from rawextensions
    This will mainly be used in Syncer when comparing
    PolicyNode/ClusterPolicy resources to actual resources on the cluster.
    
    Bug: 115391815
    Change-Id: Idca9c0e81ee3e406a84ce2362d296555bbb077a2

---
+++ pkg/generic-syncer/decode/decode.go
@@ -0,0 +1,67 @@
+/*
+Copyright 2018 The Nomos Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package decode is used for decoding serialized data in Nomos resources.
+package decode
+
+import (
+	"fmt"
+
+	nomosv1 "github.com/google/nomos/pkg/api/policyhierarchy/v1"
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+)
+
+// Decoder decodes GenericResources from PolicyNodes / ClusterPolicies to
+// Unstructured structs.
+type Decoder struct {
+	decoder runtime.Decoder
+}
+
+// NewDecoder returns a new Decoder.
+func NewDecoder(scheme *runtime.Scheme) *Decoder {
+	return &Decoder{
+		decoder: serializer.NewCodecFactory(scheme).UniversalDeserializer(),
+	}
+}
+
+// DecodeResources reads the bytes in the RawExtensions representing k8s
+// resources and returns a slice of all the resources grouped by their
+// respective GroupVersionKind.
+func (d *Decoder) DecodeResources(genericResources ...nomosv1.GenericResources) (map[schema.GroupVersionKind][]*unstructured.Unstructured, error) {
+	us := make(map[schema.GroupVersionKind][]*unstructured.Unstructured)
+	for _, gr := range genericResources {
+		for _, v := range gr.Versions {
+			for _, genericObject := range v.Objects {
+				gvk := schema.GroupVersionKind{Group: gr.Group, Version: v.Version, Kind: gr.Kind}
+				var o runtime.Object
+				o, _, err := d.decoder.Decode(genericObject.Raw, &gvk, o)
+				if err != nil {
+					return nil, errors.Wrapf(err, "could not decode runtime.Object from %q RawExtension bytes", gvk)
+				}
+				au, ok := o.(*unstructured.Unstructured)
+				if !ok {
+					return nil, fmt.Errorf("could not treat GenericResource object %q as an unstructured.Unstructured", gvk)
+				}
+				us[gvk] = append(us[gvk], au)
+			}
+		}
+	}
+	return us, nil
+}
---
+++ pkg/generic-syncer/decode/decode_test.go
@@ -0,0 +1,156 @@
+/*
+Copyright 2018 The Nomos Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package decode
+
+import (
+	"testing"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"reflect"
+
+	"github.com/davecgh/go-spew/spew"
+	nomosv1 "github.com/google/nomos/pkg/api/policyhierarchy/v1"
+)
+
+func TestDecodeResources(t *testing.T) {
+	testCases := []struct {
+		name            string
+		genericResource []nomosv1.GenericResources
+		want            map[schema.GroupVersionKind][]*unstructured.Unstructured
+	}{
+		{
+			name: "no resource",
+			genericResource: []nomosv1.GenericResources{
+				{
+					Kind:     "ObjectTest",
+					Versions: []nomosv1.GenericVersionResources{},
+				},
+			},
+			want: map[schema.GroupVersionKind][]*unstructured.Unstructured{},
+		},
+		{
+			name: "one resource",
+			genericResource: []nomosv1.GenericResources{
+				{
+					Kind: "ObjectTest",
+					Versions: []nomosv1.GenericVersionResources{{
+						Version: "v1test",
+						Objects: []runtime.RawExtension{{
+							Raw: []byte(`{"kind":"ObjectTest","apiVersion":"v1test"}`),
+						}},
+					}},
+				},
+			},
+			want: map[schema.GroupVersionKind][]*unstructured.Unstructured{
+				schema.GroupVersionKind{Group: "", Version: "v1test", Kind: "ObjectTest"}: {
+					&unstructured.Unstructured{
+						Object: map[string]interface{}{
+							"kind":       "ObjectTest",
+							"apiVersion": "v1test",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "multiple resources",
+			genericResource: []nomosv1.GenericResources{
+				{
+					Kind: "ObjectTest",
+					Versions: []nomosv1.GenericVersionResources{{
+						Version: "v1test",
+						Objects: []runtime.RawExtension{{
+							Raw: []byte(`{"kind":"ObjectTest","apiVersion":"v1test"}`),
+						}},
+					}},
+				},
+				{
+					Group: "foo.com",
+					Kind:  "Bar",
+					Versions: []nomosv1.GenericVersionResources{{
+						Version: "v1",
+						Objects: []runtime.RawExtension{
+							{
+								Raw: []byte(`{"kind":"Bar","apiVersion":"foo.com/v1","metadata":{"name": "my-bar"},"spec":{"foo":"baz"}}`),
+							},
+							{
+								Raw: []byte(`{"kind":"Bar","apiVersion":"foo.com/v1","metadata":{"name": "my-other-bar"},
+"spec":{"foo":"baz"}}`),
+							},
+						},
+					}},
+				},
+			},
+			want: map[schema.GroupVersionKind][]*unstructured.Unstructured{
+				schema.GroupVersionKind{Group: "", Version: "v1test", Kind: "ObjectTest"}: {
+					&unstructured.Unstructured{
+						Object: map[string]interface{}{
+							"kind":       "ObjectTest",
+							"apiVersion": "v1test",
+						},
+					},
+				},
+				schema.GroupVersionKind{Group: "foo.com", Version: "v1", Kind: "Bar"}: {
+					&unstructured.Unstructured{
+						Object: map[string]interface{}{
+							"kind":       "Bar",
+							"apiVersion": "foo.com/v1",
+							"metadata": map[string]interface{}{
+								"name": "my-bar",
+							},
+							"spec": map[string]interface{}{
+								"foo": "baz",
+							},
+						},
+					},
+					&unstructured.Unstructured{
+						Object: map[string]interface{}{
+							"kind":       "Bar",
+							"apiVersion": "foo.com/v1",
+							"metadata": map[string]interface{}{
+								"name": "my-other-bar",
+							},
+							"spec": map[string]interface{}{
+								"foo": "baz",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			scheme := runtime.NewScheme()
+			for w := range tc.want {
+				scheme.AddKnownTypeWithName(w, &unstructured.Unstructured{})
+			}
+			d := NewDecoder(scheme)
+			got, err := d.DecodeResources(tc.genericResource...)
+			if err != nil {
+				t.Errorf("Could not decode: %v", err)
+			}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("got: %v\nwant: %v", spew.Sdump(got), spew.Sdump(tc.want))
+			}
+		})
+	}
+}
