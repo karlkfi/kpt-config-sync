3d830950ac7b2a9070f27413c7ba3916061e67fa
Filip Filmar <fmil@google.com>
2018-10-30 16:32:45 -0700

    oidc: more env variable fixing
    - I realized that the previous env variable name for --cluster-config
    was wrong, so reverted.
    
    - Also realized that now it is not possible to load a kubeconfig file
    implicitly, so added that.  Now, we can either specify explicitly, or
    get it from KUBECONFIG env variable (this is kubectl standard) or get it
    from user's default kubeconfig file.
    
    Change-Id: Id6e881ec685c83b7885647992bbe09e221ab1bce

--- pkg/oidc/oidc.go
+++ pkg/oidc/oidc.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/user"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -16,6 +18,21 @@ var rootCmd = &cobra.Command{
 	Long:  "Long description",
 	Run: func(cmd *cobra.Command, args []string) {
 	},
+	PreRunE: func(_ *cobra.Command, _ []string) error {
+		if kubeConfig == "" {
+			// Try to load the default user kubeconfig file if not specified.
+			// This requires cgo so may fail in some statically linked environments.
+			user, err := user.Current()
+			if err != nil {
+				return fmt.Errorf("could not determine current user: %v", err)
+			}
+			kubeConfig = filepath.Join(user.HomeDir, ".kube", "config")
+		}
+		if _, err := os.Stat(kubeConfig); err != nil {
+			return fmt.Errorf("could not find config file: %q", kubeConfig)
+		}
+		return nil
+	},
 }
 
 var kubeConfig string
@@ -24,7 +41,7 @@ func init() {
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 
 	rootCmd.PersistentFlags().StringVar(&kubeConfig,
-		"kube-config", os.Getenv("KUBECTL_PLUGINS_LOCAL_FLAG_KUBE_CONFIG"),
+		"kube-config", os.Getenv("KUBECONFIG"),
 		"the path to a Kubernetes configuration file.  If undefined, defaults are used.")
 }
 
