69e5c702e0c1d24cb7a93e62ae39b865e4745d8d
Will Beason <willbeason@willbeason.sfo.corp.google.com>
2018-11-01 13:38:08 -0700

    Add registry for KubernetesNomosVet errors.
    Also add copyright statement at top of file.
    
    Change-Id: I54b9b0c1f2a5c6541d1e6b95e23660157e965db6

--- pkg/policyimporter/analyzer/validation/errors.go
+++ pkg/policyimporter/analyzer/validation/errors.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2017 The Nomos Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package validation
 
 import (
@@ -12,6 +27,40 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// code returns the unique code associated with the error type.
+// Only ever (1) add to this method or (2) deprecate ids. Do not reuse.
+func code(e error) string {
+	switch e.(type) {
+	case DuplicateDirectoryNameError:
+		return "1002"
+	case IllegalNamespaceChildDirectoryError:
+		return "1003"
+	case IllegalNamespaceSelectorAnnotationError:
+		return "1004"
+	case UnsyncableClusterObjectError:
+		return "1005"
+	case UnsyncableNamespaceObjectError:
+		return "1006"
+	case IllegalAbstractNamespaceObjectKindError:
+		return "1007"
+	case ConflictingResourceQuotaError:
+		return "1008"
+	case IllegalNamespaceDeclarationError:
+		return "1009"
+	case IllegalAnnotationDefinitionError:
+		return "1010"
+	case IllegalLabelDefinitionError:
+		return "1011"
+	default:
+		panic("Unknown Nomosvet Error Type") // Undefined
+	}
+}
+
+// withPrefix formats the start of error messages consistently.
+func format(err error, format string, a ...interface{}) string {
+	return fmt.Sprintf("Nomosvet Error KNV%s: ", code(err)) + fmt.Sprintf(format, a...)
+}
+
 // ReservedDirectoryNameError represents an illegal usage of a reserved name.
 type ReservedDirectoryNameError struct {
 	*ast.TreeNode
@@ -19,8 +68,8 @@ type ReservedDirectoryNameError struct {
 
 // Error implements error.
 func (e ReservedDirectoryNameError) Error() string {
-	return fmt.Sprintf(
-		"KNV1001: Directories MUST NOT have reserved namespace names. "+
+	return format(e,
+		"Directories MUST NOT have reserved namespace names. "+
 			"Rename or remove directory %[1]q from %[4]s/%[3]s:\n"+
 			"%[2]s",
 		e.Name(), e.TreeNode, v1alpha1.ReservedNamespacesConfigMapName, repo.SystemDir)
@@ -34,8 +83,8 @@ type DuplicateDirectoryNameError struct {
 
 // Error implements error.
 func (e DuplicateDirectoryNameError) Error() string {
-	return fmt.Sprintf(
-		"KNV1002: Directory names MUST be unique. "+
+	return format(e,
+		"Directory names MUST be unique. "+
 			"Rename one of these two directories:\n"+
 			"%[1]s\n\n"+
 			"%[2]s",
@@ -50,8 +99,8 @@ type IllegalNamespaceChildDirectoryError struct {
 
 // Error implements error.
 func (e IllegalNamespaceChildDirectoryError) Error() string {
-	return fmt.Sprintf(
-		"KNV1003: A %[1]s directory MUST NOT have children. "+
+	return format(e,
+		"A %[1]s directory MUST NOT have children. "+
 			"Restructure %[4]s so that it does not have child %[2]q:\n"+
 			"%[3]s",
 		ast.Namespace, e.child.Name(), e.child, e.parent.Name())
@@ -64,8 +113,8 @@ type IllegalNamespaceSelectorAnnotationError struct {
 
 // Error implements error.
 func (e IllegalNamespaceSelectorAnnotationError) Error() string {
-	return fmt.Sprintf(
-		"KNV1004: A %[3]s MUST NOT use the annotation %[2]s. "+
+	return format(e,
+		"A %[3]s MUST NOT use the annotation %[2]s. "+
 			"Remove metadata.annotations.%[2]s from:\n"+
 			"%[1]s",
 		e.TreeNode, v1alpha1.NamespaceSelectorAnnotationKey, ast.Namespace)
@@ -78,8 +127,8 @@ type UnsyncableClusterObjectError struct {
 
 // Error implements error.
 func (e UnsyncableClusterObjectError) Error() string {
-	return fmt.Sprintf(
-		"KNV1005: Object %[4]q defined in %[1]s/ is not syncable. "+
+	return format(e,
+		"Object %[4]q defined in %[1]s/ is not syncable. "+
 			"Enable sync for objects of kind %[2]s.\n"+
 			"%[3]s",
 		repo.ClusterDir, e.GroupVersionKind(), e.FileObject, e.Name())
@@ -92,8 +141,8 @@ type UnsyncableNamespaceObjectError struct {
 
 // Error implements error.
 func (e UnsyncableNamespaceObjectError) Error() string {
-	return fmt.Sprintf(
-		"KNV1006: Object %[4]q is not syncable. "+
+	return format(e,
+		"Object %[4]q is not syncable. "+
 			"Enable sync for objects of kind %[2]s.\n"+
 			"%[3]s",
 		repo.ClusterDir, e.GroupVersionKind(), e.FileObject, e.Name())
@@ -106,8 +155,8 @@ type IllegalAbstractNamespaceObjectKindError struct {
 
 // Error implements error.
 func (e IllegalAbstractNamespaceObjectKindError) Error() string {
-	return fmt.Sprintf(
-		"KNV1007: Objects of kind %[1]s MUST NOT be delcared in %[2]s directories. \n"+
+	return format(e,
+		"Objects of kind %[1]s MUST NOT be delcared in %[2]s directories. \n"+
 			"Move object %[5]q to a %[3]s directory:\n"+
 			"%[4]s",
 		e.GroupVersionKind(), ast.AbstractNamespace, ast.Namespace, e.FileObject, e.Name())
@@ -120,8 +169,8 @@ type ConflictingResourceQuotaError struct {
 
 // Error implements error.
 func (e ConflictingResourceQuotaError) Error() string {
-	return fmt.Sprintf(
-		"KNV1008: A directory MUST NOT contain more than one ResourceQuota object. "+
+	return format(e,
+		"A directory MUST NOT contain more than one ResourceQuota object. "+
 			"Directory %[1]q contains multiple ResourceQuota object definitions, including:\n"+
 			"%[2]s",
 		path.Dir(e.Source), e.FileObject)
@@ -135,8 +184,8 @@ type IllegalNamespaceDeclarationError struct {
 // Error implements error.
 func (e IllegalNamespaceDeclarationError) Error() string {
 	// TODO(willbeason): Error unused until b/118715158
-	return fmt.Sprintf(
-		"KNV1009: Objects MUST NOT delcare metadata.namespace. "+
+	return format(e,
+		"Objects MUST NOT delcare metadata.namespace. "+
 			"Object %[2]q declares metadata.namespace:\n"+
 			"%[1]s",
 		e.FileObject, e.Name())
@@ -151,8 +200,8 @@ type IllegalAnnotationDefinitionError struct {
 // Error implements error.
 func (e IllegalAnnotationDefinitionError) Error() string {
 	a := strings.Join(e.annotations, ", ")
-	return fmt.Sprintf(
-		"KNV1010: Objects MUST NOT define unsupported annotations starting with %[3]q. "+
+	return format(e,
+		"Objects MUST NOT define unsupported annotations starting with %[3]q. "+
 			"Object %[4]q has offending annotations: %[1]s\n%[2]s",
 		a, e.object, policyhierarchy.GroupName, e.object.Name())
 }
@@ -166,8 +215,8 @@ type IllegalLabelDefinitionError struct {
 // Error implements error.
 func (e IllegalLabelDefinitionError) Error() string {
 	l := strings.Join(e.labels, ", ")
-	return fmt.Sprintf(
-		"KNV1011: Objects MUST NOT define labels starting with %[3]q. "+
+	return format(e,
+		"Objects MUST NOT define labels starting with %[3]q. "+
 			"Object  %[3]s has these offending labels: %[1]s\n%[2]s",
 		l, e.object, policyhierarchy.GroupName, e.object.Name())
 }
