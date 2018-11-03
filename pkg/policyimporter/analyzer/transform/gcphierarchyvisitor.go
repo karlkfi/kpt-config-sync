c223a1cea1f6f0ad95343602603f753357d2c362
Tony Tseng <ttt@google.com>
2018-11-02 17:30:53 -0700

    [bespin] Add a visitor for GCP org/folder/project hierarchy
    This is based on https://team-review.git.corp.google.com/c/gke-kubernetes-enterprise-control/prototype/+/346152.
    
    This change adds a visitor for Bespin that moves organization and folder
    from namespace scope to cluster scope. It also sets up the parent
    references for GCP objects in the hierarchy (project -> folder,
    project -> org, folder -> folder, folder -> org).
    
    The error checking here is pretty rudimentary - it's just some basic sanity
    checks. The plan is to have a more thorough validation visitor later.
    
    Change-Id: If22e6abaaa67a92142069cb8098c552cce2fa658

---
+++ pkg/policyimporter/analyzer/transform/gcphierarchyvisitor.go
@@ -0,0 +1,190 @@
+/*
+Copyright 2018 The Nomos Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package transform
+
+import (
+	"github.com/golang/glog"
+	"github.com/google/nomos/pkg/api/policyascode/v1"
+	"github.com/google/nomos/pkg/policyimporter/analyzer/ast"
+	"github.com/google/nomos/pkg/policyimporter/analyzer/visitor"
+	"github.com/google/nomos/pkg/util/multierror"
+	"github.com/pkg/errors"
+)
+
+// GCPHierarchyVisitor sets up the hierarchical relationship between
+// GCP resources. It does the following:
+// * Move organization and folder from namespace scope to cluster scope.
+// * Set up parent reference between project, folder, and organization.
+type GCPHierarchyVisitor struct {
+	*visitor.Copying
+	cluster *ast.Cluster
+	ctx     *gcpHierarchyContext
+	// cumulative errors encountered by the visitor
+	errs multierror.Builder
+}
+
+var _ ast.Visitor = &GCPHierarchyVisitor{}
+
+// NewGCPHierarchyVisitor makes a new visitor.
+func NewGCPHierarchyVisitor() *GCPHierarchyVisitor {
+	v := &GCPHierarchyVisitor{Copying: visitor.NewCopying()}
+	v.SetImpl(v)
+	return v
+}
+
+// Error implements CheckingVisitor.
+// The error checking in this visitor is rudimentary.
+// The expectation is that a thorough error checking is done by another
+// validation visitor.
+func (v *GCPHierarchyVisitor) Error() error {
+	return v.errs.Build()
+}
+
+// VisitCluster implements Visitor.
+func (v *GCPHierarchyVisitor) VisitCluster(c *ast.Cluster) ast.Node {
+	newC := v.Copying.VisitCluster(c).(*ast.Cluster)
+	v.cluster = newC
+	return newC
+}
+
+// VisitReservedNamespaces implements Visitor. Currently unused and always returns
+// the passed node.
+func (v *GCPHierarchyVisitor) VisitReservedNamespaces(r *ast.ReservedNamespaces) ast.Node {
+	return r
+}
+
+// gcpHierarchyContext stores the objects seen as the tree is traversed.
+type gcpHierarchyContext struct {
+	prev *gcpHierarchyContext
+	// There should be at most 1 cluster level object per TreeNode.
+	// It can be either Organization or Folder.
+	clusterObj *ast.ClusterObject
+}
+
+// VisitTreeNode visits a tree node.
+// If the object is a folder or organization, it is added as a cluster level
+// resource instead.
+func (v *GCPHierarchyVisitor) VisitTreeNode(n *ast.TreeNode) ast.Node {
+	ctx := &gcpHierarchyContext{
+		prev: v.ctx,
+	}
+	v.ctx = ctx
+
+	// Call c.Copying.VisitTreeNode to continue iteration.
+	newNode := v.Copying.VisitTreeNode(n).(*ast.TreeNode)
+
+	if v.ctx.clusterObj != nil {
+		glog.Infof("Moving %s to cluster scope", v.ctx.clusterObj)
+		v.cluster.Objects = append(v.cluster.Objects, v.ctx.clusterObj)
+	}
+
+	// TODO(b/117675963): Process {project, folder, org} <-> IAM binding parent refs.
+
+	v.ctx = ctx.prev
+	return newNode
+}
+
+// VisitObject sets up parent references and removes folder/organization from
+// namespace object list.
+func (v *GCPHierarchyVisitor) VisitObject(o *ast.NamespaceObject) ast.Node {
+	gvk := o.GetObjectKind().GroupVersionKind()
+	if gvk.Group != v1.SchemeGroupVersion.Group {
+		return o
+	}
+	switch gcpObj := o.FileObject.Object.(type) {
+	case *v1.Project:
+		pr := v.parentReference()
+		if pr == nil {
+			v.errs.Add(errors.Errorf("GCP project %v is missing its parent.", gcpObj.Name))
+			return nil
+		}
+		if pr.Kind != "Folder" && pr.Kind != "Organization" {
+			v.errs.Add(errors.Errorf("Project %v must have either folder or org as its parent: %v", gcpObj, pr))
+			return nil
+		}
+		p := gcpObj.DeepCopy()
+		p.Spec.ParentReference = *pr
+		return &ast.NamespaceObject{
+			FileObject: ast.FileObject{
+				Object: p,
+				Source: o.Source,
+			},
+		}
+	case *v1.Folder:
+		if v.ctx.clusterObj != nil {
+			v.errs.Add(errors.Errorf("Invalid hierarchy: %v and %v ", v.ctx.clusterObj, gcpObj))
+			return nil
+		}
+		pr := v.parentReference()
+		if pr == nil {
+			if v.ctx.prev == nil {
+				v.ctx.clusterObj = &ast.ClusterObject{
+					FileObject: o.FileObject,
+				}
+			} else {
+				v.errs.Add(errors.Errorf("Folder %v is missing its parent", gcpObj))
+			}
+			return nil
+		}
+		if pr.Kind != "Folder" && pr.Kind != "Organization" {
+			v.errs.Add(errors.Errorf("Folder %v must have either folder or org as its parent: %v", gcpObj, pr))
+			return nil
+		}
+		f := gcpObj.DeepCopy()
+		f.Spec.ParentReference = *pr
+		v.ctx.clusterObj = &ast.ClusterObject{
+			FileObject: ast.FileObject{
+				Object: f,
+				Source: o.Source,
+			},
+		}
+		return nil
+	case *v1.Organization:
+		if v.ctx.clusterObj != nil {
+			v.errs.Add(errors.Errorf("Invalid hierarchy: %v and %v ", v.ctx.clusterObj, gcpObj))
+			return nil
+		}
+		if v.ctx.prev != nil {
+			v.errs.Add(errors.Errorf("Organization must be at root level"))
+			return nil
+		}
+		v.ctx.clusterObj = &ast.ClusterObject{
+			FileObject: o.FileObject,
+		}
+		return nil
+	}
+
+	return o
+}
+
+// parentReference returns the name and kind that point to the parent GCP
+// folder/organization, or nil if there is no parent.
+func (v *GCPHierarchyVisitor) parentReference() *v1.ParentReference {
+	if v.ctx.prev == nil || v.ctx.prev.clusterObj == nil {
+		return nil
+	}
+	gvk := v.ctx.prev.clusterObj.GetObjectKind().GroupVersionKind()
+	return &v1.ParentReference{
+		Kind: gvk.Kind,
+		Name: v.ctx.prev.clusterObj.Name(),
+	}
+}
+
+// VisitObjectList visits the object list.
+func (v *GCPHierarchyVisitor) VisitObjectList(o ast.ObjectList) ast.Node {
+	return v.Copying.VisitObjectList(o)
+}
---
+++ pkg/policyimporter/analyzer/transform/gcphierarchyvisitor_test.go
@@ -0,0 +1,380 @@
+/*
+Copyright 2018 The Nomos Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package transform
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/nomos/pkg/api/policyascode/v1"
+	"github.com/google/nomos/pkg/policyimporter/analyzer/ast"
+	visitorpkg "github.com/google/nomos/pkg/policyimporter/analyzer/visitor"
+	vt "github.com/google/nomos/pkg/policyimporter/analyzer/visitor/testing"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	project = &v1.Project{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: v1.SchemeGroupVersion.String(),
+			Kind:       "Project",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "project-sample",
+		},
+		Spec: v1.ProjectSpec{
+			Name: "project-sample-name",
+		},
+	}
+	folder = &v1.Folder{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: v1.SchemeGroupVersion.String(),
+			Kind:       "Folder",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "folder-sample",
+		},
+		Spec: v1.FolderSpec{
+			DisplayName: "folder-sample-name",
+		},
+	}
+	subFolder = &v1.Folder{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: v1.SchemeGroupVersion.String(),
+			Kind:       "Folder",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "subfolder-sample",
+		},
+		Spec: v1.FolderSpec{
+			DisplayName: "sub-folder-sample-name",
+		},
+	}
+	org = &v1.Organization{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: v1.SchemeGroupVersion.String(),
+			Kind:       "Organization",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "org-sample",
+		},
+	}
+)
+
+func TestFolderAndOrg(t *testing.T) {
+	folderUnderOrg := folder.DeepCopy()
+	folderUnderOrg.Spec.ParentReference = v1.ParentReference{
+		Kind: org.TypeMeta.Kind,
+		Name: org.ObjectMeta.Name,
+	}
+	subFolderUnderFolder := subFolder.DeepCopy()
+	subFolderUnderFolder.Spec.ParentReference = v1.ParentReference{
+		Kind: folder.TypeMeta.Kind,
+		Name: folder.ObjectMeta.Name,
+	}
+
+	var tests = []struct {
+		name  string
+		input *ast.Root
+		want  ast.ClusterObjectList
+	}{
+		// These tests need Cluster initialized or bad things happen.
+		{
+			name: "Organization should be cluster scoped",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Objects: vt.ObjectSets(org),
+				},
+			},
+			want: vt.ClusterObjectSets(org),
+		},
+		{
+			name: "Folder should be cluster scoped",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Objects: vt.ObjectSets(folder),
+				},
+			},
+			want: vt.ClusterObjectSets(folder),
+		},
+		{
+			name: "Folder under an organization should be cluster scoped with parent reference",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Objects: vt.ObjectSets(org),
+					Children: []*ast.TreeNode{
+						&ast.TreeNode{
+							Type:    ast.AbstractNamespace,
+							Objects: vt.ObjectSets(folder),
+						},
+					},
+				},
+			},
+			want: vt.ClusterObjectSets(folderUnderOrg, org),
+		},
+		{
+			name: "Folder under another folder should be cluster scoped with parent reference",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Objects: vt.ObjectSets(folder),
+					Children: []*ast.TreeNode{
+						&ast.TreeNode{
+							Type:    ast.AbstractNamespace,
+							Objects: vt.ObjectSets(subFolder),
+						},
+					},
+				},
+			},
+			want: vt.ClusterObjectSets(subFolderUnderFolder, folder),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// This was copied from MutatingVisitorTest and then modified.
+			// This removes much of the implementation detail and Nomos-specific
+			// code, which makes it a bit clearer what is going on for Bespin's
+			// needs.
+			copier := visitorpkg.NewCopying()
+			copier.SetImpl(copier)
+			inputCopy, ok := tc.input.Accept(copier).(*ast.Root)
+			if !ok {
+				t.Fatalf(
+					"framework error: return value from copying visitor needs to be of type *ast.Root, got: %#v", inputCopy)
+			}
+			visitor := NewGCPHierarchyVisitor()
+			output := tc.input.Accept(visitor).(*ast.Root)
+			verifyInputUnmodified(t, tc.input, inputCopy)
+			if err := visitor.Error(); err != nil {
+				t.Errorf("GCP hierarchy visitor resulted in error: %v", err)
+			}
+			if diff := cmp.Diff(tc.want, output.Cluster.Objects); diff != "" {
+				t.Errorf("got diff:\n%v", diff)
+			}
+		})
+	}
+}
+
+func TestProject(t *testing.T) {
+	projectUnderOrg := project.DeepCopy()
+	projectUnderOrg.Spec.ParentReference = v1.ParentReference{
+		Kind: org.TypeMeta.Kind,
+		Name: org.ObjectMeta.Name,
+	}
+	projectUnderFolder := project.DeepCopy()
+	projectUnderFolder.Spec.ParentReference = v1.ParentReference{
+		Kind: folder.TypeMeta.Kind,
+		Name: folder.ObjectMeta.Name,
+	}
+
+	var tests = []struct {
+		name  string
+		input *ast.Root
+		want  ast.ObjectList
+	}{
+		{
+			name: "Project under organization should be namespace scoped with parent reference",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Objects: vt.ObjectSets(org),
+					Children: []*ast.TreeNode{
+						&ast.TreeNode{
+							Type:    ast.AbstractNamespace,
+							Objects: vt.ObjectSets(project),
+						},
+					},
+				},
+			},
+			want: vt.ObjectSets(projectUnderOrg),
+		},
+		{
+			name: "Project under folder should be namespace scoped with parent reference",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Objects: vt.ObjectSets(folder),
+					Children: []*ast.TreeNode{
+						&ast.TreeNode{
+							Type:    ast.AbstractNamespace,
+							Objects: vt.ObjectSets(project),
+						},
+					},
+				},
+			},
+			want: vt.ObjectSets(projectUnderFolder),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// This was copied from MutatingVisitorTest and then modified.
+			// This removes much of the implementation detail and Nomos-specific
+			// code, which makes it a bit clearer what is going on for Bespin's
+			// needs.
+			copier := visitorpkg.NewCopying()
+			copier.SetImpl(copier)
+			inputCopy, ok := tc.input.Accept(copier).(*ast.Root)
+			if !ok {
+				t.Fatalf(
+					"framework error: return value from copying visitor needs to be of type *ast.Root, got: %#v", inputCopy)
+			}
+			visitor := NewGCPHierarchyVisitor()
+			output := tc.input.Accept(visitor).(*ast.Root)
+			verifyInputUnmodified(t, tc.input, inputCopy)
+			if output.Tree == nil || len(output.Tree.Children) != 1 {
+				t.Fatalf("unexpected output root: %+v", output)
+			}
+			projectNode := output.Tree.Children[0]
+			if diff := cmp.Diff(tc.want, projectNode.Objects); diff != "" {
+				t.Errorf("got diff:\n%v", diff)
+			}
+		})
+	}
+}
+
+func TestHierarchyError(t *testing.T) {
+	project2 := project.DeepCopy()
+	project2.ObjectMeta.Name = "project2"
+	var tests = []struct {
+		name  string
+		input *ast.Root
+	}{
+		{
+			name: "Project w/o parent",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Objects: vt.ObjectSets(project),
+				},
+			},
+		},
+		{
+			name: "Project w/o a GCP folder/org parent",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Children: []*ast.TreeNode{
+						&ast.TreeNode{
+							Type:    ast.AbstractNamespace,
+							Objects: vt.ObjectSets(project),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Project with project parent",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Objects: vt.ObjectSets(project),
+					Children: []*ast.TreeNode{
+						&ast.TreeNode{
+							Type:    ast.AbstractNamespace,
+							Objects: vt.ObjectSets(project2),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Folder with project parent",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Objects: vt.ObjectSets(project),
+					Children: []*ast.TreeNode{
+						&ast.TreeNode{
+							Type:    ast.AbstractNamespace,
+							Objects: vt.ObjectSets(folder),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Org with parent",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Children: []*ast.TreeNode{
+						&ast.TreeNode{
+							Type:    ast.AbstractNamespace,
+							Objects: vt.ObjectSets(org),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Gap in hierarchy",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Objects: vt.ObjectSets(org),
+					Children: []*ast.TreeNode{
+						&ast.TreeNode{
+							Type: ast.AbstractNamespace,
+							Children: []*ast.TreeNode{
+								&ast.TreeNode{
+									Type:    ast.AbstractNamespace,
+									Objects: vt.ObjectSets(folder),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Cannot have both org and folder at the same level",
+			input: &ast.Root{
+				Cluster: &ast.Cluster{},
+				Tree: &ast.TreeNode{
+					Type:    ast.AbstractNamespace,
+					Objects: vt.ObjectSets(org, folder),
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			visitor := NewGCPHierarchyVisitor()
+			_ = tc.input.Accept(visitor).(*ast.Root)
+			if err := visitor.Error(); err == nil {
+				t.Fatal("expected error, got nil")
+			}
+		})
+	}
+}
+
+func verifyInputUnmodified(t *testing.T, input, inputCopy *ast.Root) {
+	versionCmp := cmp.Comparer(func(lhs, rhs resource.Quantity) bool {
+		return lhs.Cmp(rhs) == 0
+	})
+	// Mutation indicates something was implemented wrong, the input shouldn't be modified.
+	if diff := cmp.Diff(input, inputCopy, versionCmp); diff != "" {
+		t.Errorf("input mutated while running visitor: %s", diff)
+	}
+}
