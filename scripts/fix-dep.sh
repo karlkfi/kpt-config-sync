21b5551cf3e2dd4e160dffa3b75d8ff4fc0c2b9d
Stephen Bochinski <sbochins@google.com>
2018-11-05 13:56:41 -0800

    Clean up vendored dependencies
    Ran `dep ensure` + `scripts/fix-dep.sh` because this wasn't run by
    someone that added dependencies in a previous commit.
    
    Change-Id: Iaa5743dcdd8f4eccbb79e5bdf9407f7389a15765

--- scripts/fix-dep.sh
+++ scripts/fix-dep.sh
@@ -2,20 +2,20 @@
 #
 # Due to the way we add licences to things dep tries to blow up all the added
 # files and put the originals back.  This script attempts to automate cleaning
-# up all the dep license chagnes.
+# up all the dep license changes.
 #
 
 # Fix deleted METADATA
 git status | grep "deleted:.*METADATA" | sed -e 's/deleted://' | xargs git checkout
 
 # Fix deleted LICENSE
-git status | grep "deleted:.*LICENSE" | sed -e 's/deleted://' | xargs git checkout
+git status | grep -E "deleted:.*(LICENSE|license)" | sed -e 's/deleted://' | xargs git checkout
 
 # Fix modified LICENSE
-git status | grep "modified:.*LICENSE" | sed -e 's/modified://' | xargs git checkout
+git status | grep -E "modified:.*(LICENSE|license)" | sed -e 's/modified://' | xargs git checkout
 
 # Remove newly vendored LICENSE.[ext]
-git status | grep "vendor/.*LICENSE\\.[a-z]*$" | xargs rm
+git status | grep -E "vendor/.*(LICENSE|license)\\.{0,1}[a-z]*$" | xargs rm
 
 # Not needed
 rm -rf vendor/github.com/kubernetes-sigs/kubebuilder/docs/
