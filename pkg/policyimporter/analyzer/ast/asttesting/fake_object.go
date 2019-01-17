71759ae6ef6c2374701abce85cb3f2a4d0aaa905
Will Beason <willbeason@willbeason.sfo.corp.google.com>
2019-01-17 06:19:10 -0800

    Define fakeast.Object
    Creating FileObjects for testing can be arduous, usually taking many
    lines to set up. However, many tests don't need real objects, just ones
    which return a specific GroupVersionKind or have specific metadata.
    
    This CL defines testing/fakeast/NewFakeFileObject for use in testing.
    It makes it convenient to instantiate objects where all you care
    about is the GroupVersionKind and metadata.
    
    Change-Id: Ifa113700d026e5fbd5a6fa20db780fa86b81fbae

---
+++ pkg/policyimporter/analyzer/ast/asttesting/fake_object.go
@@ -0,0 +1,58 @@
+package asttesting
+
+import (
+	"github.com/google/nomos/pkg/policyimporter/analyzer/ast"
+	"github.com/google/nomos/pkg/policyimporter/filesystem/nomospath"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// NewFakeFileObject returns a fake FileObject for testing.
+//
+// gvk is the GroupVersionKind the FileObject will pretend to be.
+// path is the desired slash path relative to nomos root.
+func NewFakeFileObject(gvk schema.GroupVersionKind, path string) ast.FileObject {
+	return ast.FileObject{
+		Object:   NewFakeObject(gvk),
+		Relative: nomospath.NewFakeRelative(path),
+	}
+}
+
+// NewFakeObject returns a fake runtime.Object for testing.
+// This is for when the only things you care about are the Kind and Metadata.
+func NewFakeObject(gvk schema.GroupVersionKind) *FakeObject {
+	return &FakeObject{
+		ObjectMeta: &v1.ObjectMeta{},
+		TypeMeta: &v1.TypeMeta{
+			Kind:       gvk.Kind,
+			APIVersion: gvk.GroupVersion().String(),
+		},
+	}
+}
+
+// FakeObject implements a bare bones version of runtime.Object for testing.
+type FakeObject struct {
+	*v1.ObjectMeta
+	*v1.TypeMeta
+}
+
+var _ runtime.Object = &FakeObject{}
+
+var _ v1.Object = FakeObject{}
+
+// WithMeta returns a copy of the object with the metadata replaced with meta.
+func (o *FakeObject) WithMeta(meta *v1.ObjectMeta) *FakeObject {
+	return &FakeObject{
+		ObjectMeta: meta,
+		TypeMeta:   o.TypeMeta,
+	}
+}
+
+// DeepCopyObject implements runtime.Object.
+func (o *FakeObject) DeepCopyObject() runtime.Object {
+	return &FakeObject{
+		ObjectMeta: o.ObjectMeta.DeepCopy(),
+		TypeMeta:   o.TypeMeta,
+	}
+}
