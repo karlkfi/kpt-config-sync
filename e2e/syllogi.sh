3bb03617085f0ef7faa0f212d772ab13398eff92
Michael Druskin <mdruskin@google.com>
2019-01-24 09:49:00 -0800

    Move git-server for syllogi to nomos-release
    The syllogi-ci project is not accessible from inside the cluster under
    test.
    
    Also made a small visual change to the JUnit result to have them fit
    better in testgrid with other results.
    
    Change-Id: I906a5d18b6e7239c2b5703bae359d24cd7e073b2

--- e2e/syllogi.sh
+++ e2e/syllogi.sh
@@ -65,6 +65,8 @@ if ! "${run_bats_tests[@]}" | tee "${result_file}"; then
 fi
 
 echo "Converting test results from TAP format to jUnit"
-tap2junit --name "nomos_tests" "${result_file}"
-mv "${result_file}".xml "${REPORT_DIR}/junit_nomos.xml"
+# The tap2junit util cleans up the test name and doesn't allow []s, but all syllogi tests
+# start with "[test name]", so doing this silly thing here.
+tap2junit --name "ZZPREFIX" "${result_file}"
+sed "s/ZZPREFIX/[Nomos Addon] /" "${result_file}".xml > "${REPORT_DIR}/junit_nomos.xml"
 echo "Results at ${REPORT_DIR}/junit_nomos.xml"
