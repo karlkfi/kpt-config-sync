a089af671d24197795b0da51885242155e8366b1
Filip Filmar <fmil@google.com>
2019-04-15 16:55:26 -0700

    nomos-cli: adds new version implementation
    Adds a new implementation of `nomos version` which knows how to display
    component versions across multiple clusters.  This is useful for
    examining the currently running instace of Nomos.  The implementation
    requires that the installation populates the status.configManagement
    version field of the "config-management" resource.
    
    For details, see: go/nomos-cli-version-design.
    
    Bug: 130362849
    Change-Id: I12bbacce94f4a97ef87637a279fa8d1516b97d38

--- cmd/nomos/version/version.go
+++ cmd/nomos/version/version.go
@@ -2,19 +2,237 @@ package version
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"sort"
+	"sync"
+	"time"
 
+	"github.com/google/nomos/cmd/nomos/flags"
+	"github.com/google/nomos/pkg/client/restconfig"
 	"github.com/google/nomos/pkg/version"
 	"github.com/spf13/cobra"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+	"k8s.io/kubernetes/pkg/printers"
 )
 
-// Cmd is the Cobra object representing the nomos version command.
-var Cmd = &cobra.Command{
-	Use:   "version",
-	Short: "Prints the version of this binary",
-	Long: `Prints the version of the "nomos" client binary for debugging purposes.
+const (
+	unknown                     = "<unknown>"
+	errMsg                      = "<error>"
+	notInstalled                = "<not installed>"
+	configManagementName        = "config-management"
+	configManagementVersionName = "configManagementVersion"
+)
+
+var (
+	// Cmd is the Cobra object representing the nomos version command.
+	Cmd = &cobra.Command{
+		Use:   "version",
+		Short: "Prints the version of this binary",
+		Long: `Prints the version of the "nomos" client binary for debugging purposes.
 `,
-	Example: `  nomos version`,
-	Run: func(_ *cobra.Command, _ []string) {
-		fmt.Printf("%s\n", version.VERSION)
-	},
+		Example: `  nomos version`,
+		Run: func(_ *cobra.Command, _ []string) {
+			versionInternal(
+				restconfig.AllKubectlConfigs, os.Stdout,
+				flags.Clusters, flags.AllClusters)
+		},
+	}
+
+	// clientVersion is a function that obtains the local client version.
+	clientVersion = func() string {
+		return version.VERSION
+	}
+
+	// dynamicCLient obtains a client based on the supplied REST config.  Can
+	// be overridden in tests.
+	dynamicClient = func(c *rest.Config) (dynamic.Interface, error) {
+		c.Timeout = 30 * time.Second
+		return dynamic.NewForConfig(c)
+	}
+)
+
+// versionInternal allows stubbing out the config for tests.
+func versionInternal(
+	configs func() (map[string]*rest.Config, error),
+	w io.Writer, clusters []string, allClusters bool) {
+	if len(clusters) == 0 && !allClusters {
+		// Old behavior is to print the version of the local command and exit.
+		// nolint:errcheck
+		fmt.Fprintf(w, "%s\n", clientVersion())
+		return
+	}
+	// See go/nomos-cli-version-design for the output below.
+	allCfgs, err := configs()
+	if err != nil {
+		// nolint:errcheck
+		fmt.Fprintf(w, "could not get kubernetes config file: %v", err)
+		os.Exit(255)
+	}
+	cfgs := allCfgs
+	if !allClusters {
+		cfgs = filterConfigs(clusters, allCfgs)
+	}
+	vs := versions(cfgs)
+	es := entries(vs)
+	tabulate(es, w)
+}
+
+// filterConfigs retains from all only the configs that have been selected
+// through flag use. clusters is the list of clusters to print information for.
+// If allClusters is true, clusters is ignored and information for all clusters
+// is printed.
+func filterConfigs(clusters []string, all map[string]*rest.Config) map[string]*rest.Config {
+	cfgs := make(map[string]*rest.Config)
+	for _, name := range clusters {
+		if cfg, ok := all[name]; ok {
+			cfgs[name] = cfg
+		}
+	}
+	return cfgs
+}
+
+func client(name string, c *rest.Config) (dynamic.ResourceInterface, error) {
+	cl, err := dynamicClient(c)
+	if err != nil {
+		return nil, err
+	}
+	gvr := schema.GroupVersionResource{
+		Group:   "addons.sigs.k8s.io",
+		Version: "v1alpha1",
+		// The dynamic client needs the plural resource form to be able to
+		// construct a correct resource URL.
+		Resource: "configmanagements",
+	}
+	return cl.Resource(gvr).Namespace(""), nil
+}
+
+func lookupVersion(name string, cfg *rest.Config) (string, error) {
+	i, err := client(name, cfg)
+	if err != nil {
+		return "", err
+	}
+	u, err := i.Get(configManagementName, metav1.GetOptions{}, "")
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return notInstalled, nil
+		}
+		return errMsg, err
+	}
+	c := u.UnstructuredContent()
+	// {
+	//   ...
+	//   "status": {
+	//     ...
+	//     "configManagementVersion": "some_string",
+	//     ...
+	//   }
+	// }
+	s, ok := c["status"]
+	if !ok {
+		return errMsg, fmt.Errorf("internal error: can not parse status")
+	}
+	sp, ok := s.(map[string]interface{})
+	if !ok {
+		return errMsg, fmt.Errorf("internal error: status is not a map")
+	}
+	d, ok := sp[configManagementVersionName]
+	if !ok {
+		return unknown, nil
+	}
+	v, ok := d.(string)
+	if !ok {
+		return errMsg, fmt.Errorf("internal error: configManagementVersion is not a string")
+	}
+	if v == "" {
+		v = unknown
+	}
+	return v, nil
+}
+
+// vErr is either a version or an error.
+type vErr struct {
+	version string
+	err     error
+}
+
+// versions obtains the versions of all configmanagements from the clusters
+// supplied in the named configs.
+func versions(cfgs map[string]*rest.Config) map[string]vErr {
+	if len(cfgs) == 0 {
+		return nil
+	}
+	vs := make(map[string]vErr, len(cfgs))
+	var (
+		m sync.Mutex // GUARDS vs
+		g sync.WaitGroup
+	)
+	for n, c := range cfgs {
+		g.Add(1)
+		go func(n string, c *rest.Config) {
+			defer g.Done()
+			var ve vErr
+			ve.version, ve.err = lookupVersion(n, c)
+			m.Lock()
+			vs[n] = ve
+			m.Unlock()
+		}(n, c)
+	}
+	g.Wait()
+	return vs
+}
+
+// entry is one entry of the output
+type entry struct {
+	// name is the cluster context name.
+	name string
+	// component is the nomos component name.
+	component string
+	// vErr is either a version or an error.
+	vErr
+}
+
+// entries produces a stable list of version reports based on the unordered
+// versions provided.
+func entries(vs map[string]vErr) []entry {
+	var es []entry
+	for n, v := range vs {
+		es = append(es, entry{name: n, component: configManagementName, vErr: v})
+	}
+	// Also fill in the client version here.
+	es = append(es, entry{
+		component: "<client>",
+		vErr:      vErr{version: clientVersion(), err: nil}})
+	sort.SliceStable(es, func(i, j int) bool {
+		return es[i].name < es[j].name
+	})
+	return es
+}
+
+// tabulate prints out the findings in the provided entries in a nice tabular
+// form.  It's the sixties, go for it!
+func tabulate(es []entry, out io.Writer) {
+	const format = "%s\t%s\t%s\n"
+	w := printers.GetNewTabWriter(out)
+	defer func() {
+		if err := w.Flush(); err != nil {
+			// nolint:errcheck
+			fmt.Fprintf(os.Stderr, "error on Flush(): %v", err)
+		}
+	}()
+	// nolint:errcheck
+	fmt.Fprintf(w, format, "NAME", "COMPONENT", "VERSION")
+	for _, e := range es {
+		if e.err != nil {
+			// nolint:errcheck
+			fmt.Fprintf(w, format, e.name, e.component, fmt.Sprintf("<error: %v>", e.err))
+			continue
+		}
+		// nolint:errcheck
+		fmt.Fprintf(w, format, e.name, e.component, e.version)
+	}
 }
---
+++ cmd/nomos/version/version_test.go
@@ -0,0 +1,113 @@
+package version
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/dynamic/fake"
+	"k8s.io/client-go/rest"
+)
+
+func TestLegacyVersion(t *testing.T) {
+	tests := []struct {
+		name     string
+		version  string
+		expected string
+	}{
+		{
+			name:     "basic",
+			version:  "v1.2.3-rc.4",
+			expected: "v1.2.3-rc.4\n",
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			clientVersion = func() string {
+				return test.version
+			}
+			var b strings.Builder
+			versionInternal(nil, &b, nil, false)
+			if b.String() != test.expected {
+				t.Errorf("version()=%q, want: %q", b.String(), test.expected)
+			}
+		})
+	}
+}
+
+func TestVersion(t *testing.T) {
+	tests := []struct {
+		name        string
+		version     string
+		objects     []runtime.Object
+		expected    []string
+		clusters    []string
+		allClusters bool
+		configs     map[string]*rest.Config
+	}{
+		{
+			name:        "not installed",
+			allClusters: true,
+			version:     "v2.3.4-rc.5",
+			expected: []string{
+				"NAME     COMPONENT           VERSION",
+				"         <client>            v2.3.4-rc.5",
+				"config   config-management   <not installed>",
+				"",
+			},
+			configs: map[string]*rest.Config{"config": nil},
+		},
+		{
+			name:        "installed something",
+			allClusters: true,
+			version:     "v3.4.5-rc.6",
+			objects: []runtime.Object{
+				&unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"kind":       "ConfigManagement",
+						"apiVersion": "addons.sigs.k8s.io/v1alpha1",
+						"metadata": map[string]interface{}{
+							"name":      "config-management",
+							"namespace": "",
+						},
+						"status": map[string]interface{}{
+							"configManagementVersion": "v1.2.3-rc.42",
+						},
+					},
+				},
+			},
+			expected: []string{
+				"NAME     COMPONENT           VERSION",
+				"         <client>            v3.4.5-rc.6",
+				"config   config-management   v1.2.3-rc.42",
+				"",
+			},
+			configs: map[string]*rest.Config{"config": nil},
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			clientVersion = func() string {
+				return test.version
+			}
+			dynamicClient = func(c *rest.Config) (dynamic.Interface, error) {
+				return fake.NewSimpleDynamicClient(runtime.NewScheme(), test.objects...), nil
+			}
+			var b strings.Builder
+			c := func() (map[string]*rest.Config, error) {
+				return test.configs, nil
+			}
+			versionInternal(c, &b, test.clusters, test.allClusters)
+			actuals := strings.Split(b.String(), "\n")
+			if !cmp.Equal(actuals, test.expected) {
+				t.Errorf("version()=\n%+v\n\nwant:\n%+v\n\ndiff=\n%v",
+					b.String(), test.expected, cmp.Diff(test.expected, actuals))
+			}
+		})
+	}
+}
