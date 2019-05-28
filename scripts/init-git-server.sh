ff57c2fe97a76c49a54859fad0598ae45b134bc8
Will Beason <willbeason@google.com>
2019-05-28 08:52:56 -0700

    Remove Nomos Authors license
    This license is obsolete
    
    Change-Id: I3a5d4b34640439855742c43c7ac4cb38f8d46858

--- scripts/init-git-server.sh
+++ scripts/init-git-server.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Copyright 2018 The Nomos Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # This script initializes the test git server used during e2e tests.
 # In particular, it configures the ssh key for accessing the service using
 # the local public key, sets up ssh forwarding through localhost:2222 to
