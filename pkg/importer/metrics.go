1dc6af661ca74bb52cd44c95db2f935ca69e5dee
Erik Kitson <ekitson@google.com>
2020-03-03 10:31:52 -0800

    Remove obsolete importer metrics.
    Bug: 150383602
    Change-Id: I991417a0ef7059928fd213afa960d51061105027

--- pkg/importer/metrics.go
+++ pkg/importer/metrics.go
@@ -7,24 +7,9 @@ import (
 
 // Metrics contains the Prometheus metrics for the Importer.
 var Metrics = struct {
-	APICallDuration  *prometheus.HistogramVec
 	CycleDuration    *prometheus.HistogramVec
 	NamespaceConfigs prometheus.Gauge
-	Operations       *prometheus.CounterVec
 }{
-	APICallDuration: prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Help:      "Distribution of durations of API server calls",
-			Namespace: configmanagement.MetricsNamespace,
-			Subsystem: "importer",
-			Name:      "api_duration_seconds",
-			Buckets:   []float64{.001, .01, .1, 1},
-		},
-		// operation: create, update, delete
-		// type: namespace, cluster, sync
-		// status: success, error
-		[]string{"operation", "type", "status"},
-	),
 	CycleDuration: prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Help:      "Distribution of durations of cycles that the importer has attempted to complete",
@@ -34,18 +19,6 @@ var Metrics = struct {
 		},
 		// status: success, error
 		[]string{"status"},
-	),
-	Operations: prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Help:      "Total operations that have been performed to keep configs up-to-date with source of truth",
-			Namespace: configmanagement.MetricsNamespace,
-			Subsystem: "importer",
-			Name:      "operations_total",
-		},
-		// operation: create, update, delete
-		// type: namespace, cluster, sync
-		// status: success, error
-		[]string{"operation", "type", "status"},
 	),
 	NamespaceConfigs: prometheus.NewGauge(
 		prometheus.GaugeOpts{
@@ -59,9 +32,7 @@ var Metrics = struct {
 
 func init() {
 	prometheus.MustRegister(
-		Metrics.APICallDuration,
 		Metrics.CycleDuration,
 		Metrics.NamespaceConfigs,
-		Metrics.Operations,
 	)
 }
