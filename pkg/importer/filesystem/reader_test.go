599d0f519c92ba277a8537a0bb1913fecdafec09
Will Beason <willbeason@google.com>
2020-04-22 08:49:57 -0700

    Test file/dir error handling in FileReader
    Add tests that prove that FileReader properly handles errors it
    encounters while reading subdirectories. I've manually verified that
    reverting the preivous change causes the new
    TestFileReader_Read_BadPermissionsChild test to fail.
    
    This does not exhaustively test FileReader logic/error handling, just
    the above case and ones close to it.
    
    Bug: b/154107250
    
    Change-Id: Ie2674b3787246420c2c907bfebf2ad454077725a

---
+++ pkg/importer/filesystem/reader_test.go
@@ -0,0 +1,93 @@
+package filesystem
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/nomos/pkg/importer/filesystem/cmpath"
+)
+
+var tmpBase = filepath.Join(os.TempDir(), "nomos-test")
+
+func tempDir(t *testing.T) string {
+	err := os.MkdirAll(tmpBase, os.ModePerm)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dir, err := ioutil.TempDir(tmpBase, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	return dir
+}
+
+func asRooted(t *testing.T, path string) cmpath.RootedPath {
+	root, err := cmpath.NewRoot(cmpath.FromSlash(path))
+	if err != nil {
+		t.Fatal(err)
+	}
+	return root
+}
+
+func TestFileReader_Read_NotExist(t *testing.T) {
+	dir := tempDir(t)
+
+	// Remove directory so os.Stat returns a NotExist error.
+	err := os.RemoveAll(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reader := FileReader{}
+
+	objs, err := reader.Read(asRooted(t, dir))
+	if err != nil || len(objs) > 0 {
+		t.Errorf("got Read(nonexistent path) = %+v, %v; want nil, nil", objs, err)
+	}
+}
+
+func TestFileReader_Read_BadPermissionsParent(t *testing.T) {
+	dir := tempDir(t)
+
+	// Change permissions on the root directory so os.Stat returns a
+	// Permission error.
+	err := os.Chmod(dir, 000)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reader := FileReader{}
+
+	objs, err := reader.Read(asRooted(t, dir))
+	if err == nil || len(objs) > 0 {
+		t.Errorf("got Read(bad permissions on parent dir) = %+v, %v; want nil, error", objs, err)
+	}
+}
+
+func TestFileReader_Read_BadPermissionsChild(t *testing.T) {
+	dir := tempDir(t)
+
+	// Create subdirectory.
+	subDir := path.Join(dir, "namespaces")
+	err := os.Mkdir(subDir, os.ModePerm)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Change permissions on the subdirectory so os.Stat returns a
+	// Permission error.
+	err = os.Chmod(subDir, 000)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reader := FileReader{}
+
+	objs, err := reader.Read(asRooted(t, dir))
+	if err == nil || len(objs) > 0 {
+		t.Errorf("got Read(bad permissions on child dir) = %+v, %v; want nil, error", objs, err)
+	}
+}
