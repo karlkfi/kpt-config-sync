4b2ee0bce6d5c3ab3f3539ffb2280939a7cc0122
Erik Kitson <ekitson@google.com>
2020-05-04 08:18:19 -0700

    Add error logs for missing git files.
    Bug: 155385630
    Change-Id: I6cd2b1f03235c0b4909ecacb637b2ead0b6858c5

--- pkg/importer/filesystem/parse_file.go
+++ pkg/importer/filesystem/parse_file.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/golang/glog"
 	"github.com/google/nomos/pkg/importer"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -23,6 +24,7 @@ func parseFile(path string) ([]*unstructured.Unstructured, error) {
 	case ".yml", ".yaml":
 		contents, err := ioutil.ReadFile(path)
 		if err != nil {
+			glog.Errorf("Failed to read file declared in git from mounted filesystem: %s", path)
 			importer.Metrics.Violations.Inc()
 			return nil, err
 		}
@@ -30,6 +32,7 @@ func parseFile(path string) ([]*unstructured.Unstructured, error) {
 	case ".json":
 		contents, err := ioutil.ReadFile(path)
 		if err != nil {
+			glog.Errorf("Failed to read file declared in git from mounted filesystem: %s", path)
 			importer.Metrics.Violations.Inc()
 			return nil, err
 		}
