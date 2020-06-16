82a4475d766475deaaf7b5745ce5258fef45f28f
Erik Kitson <ekitson@google.com>
2020-06-16 16:07:39 -0700

    Add e2e test for kubectl conflicts
    Note that this test is currently testing "bad behavior" because we need
    to fix the bug referenced below. When we fix that bug, we will update
    the test to NOT expect an error.
    
    Bug: 159164014
    Change-Id: I7035f54b5a307c73d5f1cbdb1c5532384ad779a3

--- e2e/nomostest/predicates.go
+++ e2e/nomostest/predicates.go
@@ -23,3 +23,17 @@ func WrongTypeErr(got, want interface{}) error {
 // ErrFailedPredicate indicates the the object on the API server does not match
 // the Predicate.
 var ErrFailedPredicate = errors.New("failed predicate")
+
+// HasLabel returns a predicate that tests if an Object has the specified label key/value pair.
+func HasLabel(key, value string) Predicate {
+	return func(o core.Object) error {
+		got, okay := o.GetLabels()[key]
+		if !okay {
+			return fmt.Errorf("object %q does not have label %q; wanted %q", o.GetName(), key, value)
+		}
+		if got != value {
+			return fmt.Errorf("got %q for label %q on object %q; wanted %q", got, key, o.GetName(), value)
+		}
+		return nil
+	}
+}
---
+++ e2e/testcases/conflicts_test.go
@@ -0,0 +1,71 @@
+package e2e
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/google/nomos/e2e/nomostest"
+	"github.com/google/nomos/pkg/core"
+	"github.com/google/nomos/pkg/testing/fake"
+	corev1 "k8s.io/api/core/v1"
+)
+
+// TestConflictingKubectlApply runs a test that ensures ACM merges changes from a separate kubectl apply.
+func TestConflictingKubectlApply(t *testing.T) {
+	t.Parallel()
+	nt := nomostest.New(t)
+
+	err := nt.ValidateNotFound("foo", "", &corev1.Namespace{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	nt.Repository.Add("acme/namespaces/foo/ns.yaml", fake.NamespaceObject("foo", core.Label("hello", "world")))
+	nt.Repository.CommitAndPush("add hello:world Namespace")
+	nt.WaitForSync()
+
+	// Test that the Namespace "foo" exists with the expected label.
+	err = nt.Validate("foo", "", &corev1.Namespace{}, nomostest.HasLabel("hello", "world"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = ioutil.WriteFile(filepath.Join(nt.TmpDir, "conflict.yaml"), []byte(`
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: foo
+  labels:
+    goodnight: "moon"
+`), os.ModePerm)
+	if err != nil {
+		t.Fatalf("failed to write temporary yaml file: %v", err)
+	}
+
+	// Add a new label via kubectl.
+	exec.Command("kubectl", "--kubeconfig", filepath.Join(nt.TmpDir, "KUBECONFIG"), "apply", "-f", filepath.Join(nt.TmpDir, "conflict.yaml"))
+
+	err = nomostest.Retry(time.Second*5, func() error {
+		// Test that the Namespace "foo" exists with the manually added label.
+		err = nt.Validate("foo", "", &corev1.Namespace{}, nomostest.HasLabel("goodnight", "moon"))
+		// TODO(b/159164014): Check for error presence and return it once bug is fixed.
+		if err == nil {
+			return errors.New("unexpected success, wanted error; got nil")
+		}
+
+		// Test that the Namespace "foo" still has the label from Git.
+		err = nt.Validate("foo", "", &corev1.Namespace{}, nomostest.HasLabel("hello", "world"))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		t.Error(err)
+	}
+}
