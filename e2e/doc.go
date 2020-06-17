86e70a39941ba94f92cab7b3a493b0aee26d6f73
Will Beason <willbeason@google.com>
2020-06-17 12:29:13 -0700

    Add make targets for go e2e tests
    Add test-e2e-go which automatically builds a new Nomos image, pushes it
    to the local registry, and runs e2e tests.
    
    Add test-e2e-go-nobuild which just runs the e2e tests against the
    current image.
    
    Write docker-registry.sh that automatically configures the local docker
    registry for new users.
    
    Bug: 159013140
    
    Change-Id: Ic4d192652705ca17e97e093085e4099294b9929a

--- e2e/doc.go
+++ e2e/doc.go
@@ -1,51 +1,25 @@
 // Package e2e and its subpacakages define go e2e tests for Nomos.
 //
-// We expect this information to change as we integrate this style of test into
-// CI, so check back if things aren't working.
-//
-// TODO(b/159013140): Make a make target for these.
-//
 // Running the Tests
 //
+// A) With make, rebuilding the Nomos image:
+//
+// $ make go-e2e-test
+//
+// B) With make, without rebuilding the image:
+//
+// $ make go-e2e-test-nobuild
+//
+// C) Directly, without rebuilding:
+//
 // $ go test ./e2e/... --e2e
+//
+// Running the tests directly requires running scripts.docker-registry.sh as
+// a one-time setup step.
 //
 // You can use all of the normal `go test` flags.
 // The `--e2e` is required or else the e2e tests won't run. This lets you run
 // go test ./... to just run unit/integration tests.
-//
-//
-// //////////////
-// One-time Setup
-// //////////////
-//
-// 1) Start a local docker registry:
-// $ docker run -d --restart=always -p "5000:5000" --name kind-registry registry:2
-//
-// 2) Connect the kind cluster network to the kind-registry.
-// $ docker network connect kind kind-registry
-//
-//
-// /////////////////////////////////////
-// Testing the current state of the code
-// /////////////////////////////////////
-//
-// If you don't do this each time you modify the code, you'll be testing the
-// currently-pushed image.
-//
-// 1) Build the nomos image:
-// $ make image-nomos
-//
-// If successful, it outputs a sha like this:
-// > sha256:911ed415b430870a79af98a42b361c1fbcd52c97d1b7d1ee5884f852409d6716
-//
-// 2) Copy the sha (exclude the "sha256:" prefix), and tag the image
-// $ docker tag 911ed415b430870a79af98a42b361c1fbcd52c97d1b7d1ee5884f852409d6716 localhost:5000/nomos:latest
-//
-// 3) Push the image to the local repository:
-// $ docker push localhost:5000/nomos:latest
-//
-// 4) Run the tests
-// $ go test ./e2e/... --e2e
 //
 //
 // Debugging
--- e2e/nomostest/docker.go
+++ e2e/nomostest/docker.go
@@ -11,53 +11,39 @@ import (
 const registryName = "kind-registry"
 const registryPort = 5000
 
-// startDockerRegistry starts a local Docker registry if it does not exist.
-//
-// Adapted from https://kind.sigs.k8s.io/docs/user/local-registry/
-// Roughly equivalent to:
-// running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
-//if [ "${running}" != 'true' ]; then
-//  docker run \
-//    -d --restart=always -p "${reg_port}:5000" --name "${reg_name}" \
-//    registry:2
-//fi
+// startDockerRegistry starts a local Docker registry if it is not running.
 //
 // To manually stop the repository (for whatever reason):
 // $ docker stop kind-registry
+//
+// Assumes docker-registry.sh has already been run on the machine - otherwise
+// calls t.Fatal.
 func startLocalRegistry(nt *NT) {
+	nt.T.Helper()
+
 	// Check if the registry is already running.
 	out, err := exec.Command("docker", "inspect", "-f", "'{{.State.Running}}'", registryName).Output()
-	if err == nil {
-		switch strings.Trim(string(out), "\n'") {
-		case "true":
-			// The registry is already running, so nothing to do.
-			return
-		case "false":
-			// The registry container exists but it isn't running, so start it.
-			out, err := exec.Command("docker", "start", registryName).Output()
-			if err != nil {
-				nt.T.Logf("docker start %s out: %q", registryName, out)
-				nt.T.Fatalf("docker start %s err: %v", registryName, err)
-			}
-			return
-		default:
-			// It isn't clear how this could be reached.
-			nt.T.Fatalf("unexpected docker inspect output: %q", string(out))
-		}
-	}
-	nt.T.Logf("docker inspect out: %q", string(out))
-	nt.T.Logf("docker inspect err: %v", err)
-
-	// The registry is not active.
-	out, err = exec.Command("docker", "run", "-d", "--restart", "always",
-		"-p", fmt.Sprintf("%d:5000", registryPort), "--name", registryName, "registry:2",
-	).CombinedOutput()
 	if err != nil {
-		nt.T.Logf("docker run out: %q", string(out))
-		nt.T.Fatalf("docker run err: %v", err)
+		nt.T.Logf("docker inspect out: %q", string(out))
+		nt.T.Logf("docker inspect err: %v", err)
+		nt.T.Fatal("docker registry not configured or configured improperly; see e2e/doc.go")
 	}
-	// TODO(willbeason): Automatically shut this down after go test completes.
-	//  Note: This must be done by the caller; it can't be done here.
+	switch strings.Trim(string(out), "\n'") {
+	case "true":
+		// The registry is already running, so nothing to do.
+		return
+	case "false":
+		// The registry container exists but it isn't running, so start it.
+		out, err := exec.Command("docker", "start", registryName).Output()
+		if err != nil {
+			nt.T.Logf("docker start %s out: %q", registryName, out)
+			nt.T.Fatalf("docker start %s err: %v", registryName, err)
+		}
+		return
+	default:
+		// It isn't clear how this could be reached.
+		nt.T.Fatalf("unexpected docker inspect output: %q", string(out))
+	}
 }
 
 func connectToLocalRegistry(nt *NT) {
