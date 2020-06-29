050e517b653430ae0fe6d128dcc742e7da2e4198
juliankatz <juliankatz@google.com>
2020-06-29 14:28:05 -0700

    Create the release-commit target for postsubmit
    This target builds a nomos image and a config-sync-manifest.yaml that
    represents a successfully merged commit.
    
    The nomos image is built with VERSION set to:
    USER/BRANCH/COMMIT_SHA
    
    and pushed to:
    gcr.io/stolos-dev/nomos-commits/nomos:COMMIT_SHA
    
    This is spliced into config-sync-manifest.yaml, which is uploaded at:
    gs://stolos-dev/config-sync-manifest/commit/${COMMIT_SHA}/config-sync-manifest.yaml
    
    See demo output in this gPaste: go/paste/5887092443840512
    
    See go/cs-manifest-creation-and-release for more info.
    
    Bug: 158241701
    Change-Id: I1ddecff666296603971ead2c782ac2aa79cc52a4

---
+++ scripts/is-dirty-repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+is_dirty_repo() {
+  [ -n "$(git status --short)" ]
+  return "$?"
+}
+
+if is_dirty_repo; then
+  echo "Cannot push to commit/ from dirty repo"
+  echo "Git status output:"
+  # Prefix git status output with a pound sign for better readability
+  git status | sed 's/.*/# &/'
+  exit 1
+fi
