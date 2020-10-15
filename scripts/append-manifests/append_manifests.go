8446166ace3be3323c2731efcdd68868877ad402
Julian Katz <juliankatz@google.com>
2020-10-15 18:03:46 +0000

    Revert "Transition to using append_manifests.go"
    This reverts commit fa327bc94611a3a5c64b79b0a48981c584ebf342.
    
    Reason for revert: For some reason this is breaking in prow.  More specifically:
    
    ++++ Add yaml files to config-sync-manifest.yaml
    env GO111MODULE=on go run -mod=vendor ./scripts/append_manifests/append_manifests.go \
    	--destination=/home/prow/go/src/github.com/team-review.googlesource.com/gke-kubernetes-enterprise-control/prototype/.output/staging/operator/config-sync-manifest.yaml \
    	/home/prow/go/src/github.com/team-review.googlesource.com/gke-kubernetes-enterprise-control/prototype/.output/manifests \
    	/home/prow/go/src/github.com/team-review.googlesource.com/gke-kubernetes-enterprise-control/prototype/.output/deployment
    panic: open /home/prow/go/src/github.com/team-review.googlesource.com/gke-kubernetes-enterprise-control/prototype/.output/staging/operator/config-sync-manifest.yaml: no such file or directory
    
    Change-Id: Ib07bb66c6effcf8addb8ed4b7f8573814a07b093

--- scripts/append-manifests/append_manifests.go
+++ scripts/append-manifests/append_manifests.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"unicode/utf8"
 )
 
 const delimiter = "---\n"
@@ -56,13 +55,6 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-
-		// If a file doesn't end in a newline, add one
-		lastRune, _ := utf8.DecodeLastRuneInString(string(readBytes))
-		if lastRune != '\n' {
-			readBytes = append(readBytes, []byte("\n")...)
-		}
-
 		labeledRes := fmt.Sprintf(label, p) + string(readBytes)
 		yamlResources = append(yamlResources, labeledRes)
 	}
