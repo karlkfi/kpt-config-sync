3cc3022383d68cf3032fcc629ff5ed2405be5ce0
Will Beason <willbeason@google.com>
2021-01-26 09:46:26 -0800

    Add logic for merging Webhooks
    Add MergeWebhookConfigurations, which merges the Webhook configuration
    already on the API Server with the one required to match all of the
    types declared in the repository.
    
    Per go/nomos-acdc, we're not worrying about removing stale Rules/entries
    as it introduces more complexity than we need now, and the cost of Rules
    which are never matched is negligible.
    
    Bug: 160787495
    
    Change-Id: I1aa0e573d6da7d426276ebad816cd693b39569ab

---
+++ pkg/parse/webhook/merge.go
@@ -0,0 +1,119 @@
+package webhook
+
+import (
+	"sort"
+
+	admissionv1 "k8s.io/api/admissionregistration/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// MergeWebhookConfigurations merges two sets of ValidatingWebhookConfigurations
+// so the resulting Configuration matches the union of the initial two.
+//
+// The intent is that left is the Configuration currently on the server and
+// right is the Configuration generated from the declared configuration in a
+// repository.
+// (The logic should be symmetric, so this shouldn't have to be the case.)
+//
+// The resulting merged Configuration meets the following criteria:
+// 1) All Webhooks contain exactly one rule, matching a single GroupVersion of
+//    declared configuration.
+// 2) Webhooks are sorted by the GroupVersion they match.
+// 3) Resources within Rules are sorted alphabetically.
+//
+// Cannot return error or panic as we never want this to get stuck.
+//
+// Modifies left.
+func MergeWebhookConfigurations(left, right admissionv1.ValidatingWebhookConfiguration) admissionv1.ValidatingWebhookConfiguration {
+	webhooksMap := make(map[schema.GroupVersion]admissionv1.ValidatingWebhook)
+	for _, webhook := range append(left.Webhooks, right.Webhooks...) {
+		if len(webhook.Rules) == 0 ||
+			len(webhook.Rules[0].APIGroups) == 0 ||
+			len(webhook.Rules[0].APIVersions) == 0 {
+			// Invalid ValidatingWebhook, discard.
+			// We do NOT want to fail here as the Configuration on the API Server
+			// may have been changed by a user. We don't want to put ourselves in an
+			// infinite error loop.
+			continue
+		}
+
+		groupVersion := gv(webhook)
+		if oldWebhook, found := webhooksMap[groupVersion]; found {
+			webhooksMap[groupVersion] = mergeWebhooks(oldWebhook, webhook)
+		} else {
+			webhooksMap[groupVersion] = webhook
+		}
+	}
+
+	var webhooks []admissionv1.ValidatingWebhook
+	for _, webhook := range webhooksMap {
+		webhooks = append(webhooks, webhook)
+	}
+	sort.Slice(webhooks, func(i, j int) bool {
+		groupI := webhooks[i].Rules[0].APIGroups[0]
+		groupJ := webhooks[j].Rules[0].APIGroups[0]
+		if groupI != groupJ {
+			return groupI < groupJ
+		}
+		versionI := webhooks[i].Rules[0].APIVersions[0]
+		versionJ := webhooks[j].Rules[0].APIVersions[0]
+		return versionI < versionJ
+	})
+
+	left.Webhooks = webhooks
+	return left
+}
+
+func mergeWebhooks(left, right admissionv1.ValidatingWebhook) admissionv1.ValidatingWebhook {
+	switch {
+	case len(left.Rules) == 0:
+		left.Rules = right.Rules
+	case len(right.Rules) == 0:
+		// Nothing to merge.
+	default:
+		left.Rules[0] = mergeRule(left.Rules[0], right.Rules[0])
+	}
+	// Each Webhook should have exactly one Rule. Otherwise indicates a user
+	// has modified the Webhook in a way we don't support.
+	left.Rules = left.Rules[:1]
+
+	// FailurePolicy Ignore wins, if set. Not strictly necessary, but here to make
+	// the operation symmetric.
+	if right.FailurePolicy != nil && *right.FailurePolicy == admissionv1.Ignore {
+		ignore := admissionv1.Ignore
+		left.FailurePolicy = &ignore
+	}
+	return left
+}
+
+// mergeRule merges the two RuleWithOperations together, modifying left.
+// Returns the modified left.
+func mergeRule(left, right admissionv1.RuleWithOperations) admissionv1.RuleWithOperations {
+	// It would be more efficient to use a list merge algorithm as we know these
+	// lists are sorted, but it is more difficult to read/maintain and the
+	// performance gain is negligible.
+	resourceSet := make(map[string]bool)
+	var resources []string
+	for _, resource := range append(left.Resources, right.Resources...) {
+		if resourceSet[resource] {
+			continue
+		}
+		resourceSet[resource] = true
+		resources = append(resources, resource)
+	}
+	// Sort resources within the Rule.
+	sort.Strings(resources)
+
+	left.Resources = resources
+	return left
+}
+
+// gv gets the GroupVersion a Webhook is for.
+func gv(webhook admissionv1.ValidatingWebhook) schema.GroupVersion {
+	// To be valid, Rules must contain at least one element in APIGroups and
+	// APIVersions.
+	return schema.GroupVersion{
+		Group:   webhook.Rules[0].APIGroups[0],
+		Version: webhook.Rules[0].APIVersions[0],
+	}
+}
---
+++ pkg/parse/webhook/merge_test.go
@@ -0,0 +1,236 @@
+package webhook
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	admissionv1 "k8s.io/api/admissionregistration/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	rbacv1beta1 "k8s.io/api/rbac/v1beta1"
+)
+
+func TestMergeWebhookConfigurations(t *testing.T) {
+	// Go doesn't allow taking the address of constants.
+	ignore := admissionv1.Ignore
+
+	testCases := []struct {
+		name        string
+		left, right admissionv1.ValidatingWebhookConfiguration
+		want        admissionv1.ValidatingWebhookConfiguration
+	}{
+		{
+			name:  "empty",
+			left:  admissionv1.ValidatingWebhookConfiguration{},
+			right: admissionv1.ValidatingWebhookConfiguration{},
+			want:  admissionv1.ValidatingWebhookConfiguration{},
+		},
+		{
+			name: "one vs zero webhooks",
+			left: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			right: admissionv1.ValidatingWebhookConfiguration{},
+			want: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+		},
+		{
+			name: "duplicate webhooks",
+			left: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			right: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			want: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+		},
+		{
+			name: "different resources",
+			left: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			right: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "rolebindings"),
+				}},
+			},
+			want: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "rolebindings", "roles"),
+				}},
+			},
+		},
+		{
+			name: "different versions",
+			left: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			right: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1beta1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			want: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}, {
+					Rules: rules(rbacv1beta1.SchemeGroupVersion, "roles"),
+				}},
+			},
+		},
+		{
+			name: "different groups",
+			left: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			right: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(corev1.SchemeGroupVersion, "namespaces"),
+				}},
+			},
+			want: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(corev1.SchemeGroupVersion, "namespaces"),
+				}, {
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+		},
+		{
+			name: "honor FailurePolicy set to Ignore",
+			left: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules:         rules(rbacv1.SchemeGroupVersion, "roles"),
+					FailurePolicy: &ignore,
+				}},
+			},
+			right: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			want: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules:         rules(rbacv1.SchemeGroupVersion, "roles"),
+					FailurePolicy: &ignore,
+				}},
+			},
+		},
+		// Invalid Webhooks
+		{
+			name: "webhook missing Rules",
+			left: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			right: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{}},
+			},
+			want: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+		},
+		{
+			name: "drop webhook missing APIGroup",
+			left: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			right: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: []admissionv1.RuleWithOperations{{
+						Rule: admissionv1.Rule{
+							APIVersions: []string{"v1"},
+							Resources:   []string{"namespaces"},
+						},
+					}},
+				}},
+			},
+			want: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+		},
+		{
+			name: "drop webhook missing APIVersion",
+			left: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+			right: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: []admissionv1.RuleWithOperations{{
+						Rule: admissionv1.Rule{
+							APIGroups: []string{""},
+							Resources: []string{"namespaces"},
+						},
+					}},
+				}},
+			},
+			want: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "roles"),
+				}},
+			},
+		},
+		{
+			name: "drop extra rules",
+			left: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: append(rules(rbacv1.SchemeGroupVersion, "roles"),
+						rules(rbacv1.SchemeGroupVersion, "rolebindings")...),
+				}},
+			},
+			right: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: append(rules(rbacv1.SchemeGroupVersion, "clusterroles"),
+						rules(rbacv1.SchemeGroupVersion, "clusterrolebindings")...),
+				}},
+			},
+			want: admissionv1.ValidatingWebhookConfiguration{
+				Webhooks: []admissionv1.ValidatingWebhook{{
+					Rules: rules(rbacv1.SchemeGroupVersion, "clusterroles", "roles"),
+				}},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := MergeWebhookConfigurations(tc.left, tc.right)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Fatal(diff)
+			}
+
+			got = MergeWebhookConfigurations(tc.right, tc.left)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Fatalf("not symmetric: %s", diff)
+			}
+		})
+	}
+}
