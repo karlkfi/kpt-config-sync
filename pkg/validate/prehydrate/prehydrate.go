13fa5bcfc8b6eb9ef24dce4050ad5d18a670d901
Erik Kitson <ekitson@google.com>
2021-01-26 18:30:30 -0800

    Implement validator for directories.
    This will replace the following in nomos_parser_config.go:
    metadata.NewNamespaceDirectoryNameValidator(),
    metadata.NewMetadataNamespaceDirectoryValidator(),
    syntax.NewDirectoryNameValidator(),
    
    Note that I skipped over this validator which is obsolete:
    syntax.NewDisallowSystemObjectsValidator(),
    
    Bug: 178219594
    Change-Id: Id3a96d16194d15fc0540b9e1c9cb0d3475e1918a

---
+++ pkg/validate/prehydrate/prehydrate.go
@@ -0,0 +1,22 @@
+package prehydrate
+
+import (
+	"github.com/google/nomos/pkg/validate/parsed"
+	"github.com/google/nomos/pkg/validate/prehydrate/hierarchical"
+)
+
+// FlatValidators returns the list of visitors to validate a flat repo
+// pre-hydration.
+func FlatValidators() []parsed.ValidatorFunc {
+	return []parsed.ValidatorFunc{}
+}
+
+// HierarchicalValidators returns the list of visitors to validate a
+// hierarchical repo pre-hydration.
+func HierarchicalValidators() []parsed.ValidatorFunc {
+	return []parsed.ValidatorFunc{
+		hierarchical.NamespaceDirectoryValidator(),
+		hierarchical.ObjectDirectoryValidator(),
+		hierarchical.DirectoryNameValidator(),
+	}
+}
