eb8e3bec3e4ffd620985f2d0e7ccc56764d4c070
Jingfang Liu <jingfangliu@google.com>
2021-02-02 11:11:54 -0800

    Change prevent deletion annotation to the one in kpt live apply library
    The current prevent deletion annotation is:
    "client.lifecycle.config.k8s.io/deletion": "prevent"
    
    The standard one recommend in go/lifecycle-directives-in-detail
    and supported by kpt apply library is:
    "client.lifecycle.config.k8s.io/deletion": "detach"
    
    This change changes the value from "prevent" to "detach" since
    currently no customers are using the annotation. Also filed
    b/178865919 to update the doc.
    
    Bug: b/178019120
    Change-Id: Ie147bafe62e0e589b775075feefe80bb8a451bf2

--- pkg/lifecycle/constants.go
+++ pkg/lifecycle/constants.go
@@ -4,7 +4,10 @@
 // go/lifecycle-directives-in-detail
 package lifecycle
 
-import "github.com/google/nomos/pkg/core"
+import (
+	"github.com/google/nomos/pkg/core"
+	"sigs.k8s.io/cli-utils/pkg/common"
+)
 
 const prefix = "client.lifecycle.config.k8s.io"
 
@@ -14,10 +17,10 @@ const Deletion = prefix + "/deletion"
 
 // PreventDeletion specifies that the resource should NOT be removed from the
 // cluster if its manifest is removed from the repository.
-const PreventDeletion = "prevent"
+const PreventDeletion = common.PreventDeletion
 
 // HasPreventDeletion returns true if the object has the PreventDeletion annotation
-// and it is set to "prevent".
+// and it is set to "detach".
 func HasPreventDeletion(o core.Object) bool {
 	deletion, hasDeletion := o.GetAnnotations()[Deletion]
 	return hasDeletion && (deletion == PreventDeletion)
