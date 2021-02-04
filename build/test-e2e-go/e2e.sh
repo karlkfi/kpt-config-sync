beda17d3266321649183bfc6eb8a045c5f1fdddc
Nan Yu <nanyu@google.com>
2021-02-04 20:48:07 +0000

    Exit e2e.sh with the exit code
    The e2e.sh prints the test time in the end, which makes the script
    always exit with 0 and makes the make target always succeed even when go
    test fails.
    This commit uses trap to print the test time and exit when error occurs.
    
    This will avoid prow job showing green for failed tests.
    
    b/169356141
    
    Change-Id: Icb2016a7e2eb8a3d445979c739280c82fc8d3345

--- build/test-e2e-go/e2e.sh
+++ build/test-e2e-go/e2e.sh
@@ -3,6 +3,7 @@
 #
 # golang e2e test launcher. Do not run directly, this is intended to be executed by the prow job inside a container.
 
+set -e
 
 # Makes the service account from ${gcp_prober_cred} the active account that drives
 # cluster changes.
@@ -15,6 +16,12 @@ gcloud --quiet auth activate-service-account --key-file="${GCP_PROBER_CREDS}"
 gcloud --quiet container clusters get-credentials "${GCP_CLUSTER}" --zone="${GCP_ZONE}" --project="${GCP_PROJECT}"
 
 start_time=$(date +%s)
+
+function test_time {
+  end_time=$(date +%s)
+  echo "Tests took $(( end_time - start_time )) seconds."
+}
+
+trap test_time EXIT
+
 go test ./e2e/... --e2e "$@"
-end_time=$(date +%s)
-echo "Tests took $(( end_time - start_time )) seconds."
