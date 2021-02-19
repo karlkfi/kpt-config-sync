0f25a0990b6ac01e3811e69ba29910e51c51116e
Nan Yu <nanyu@google.com>
2021-02-19 02:30:27 +0000

    Keep access-token in the kubeconfig file
    The refresh-gcloud-creds.sh script should only remove the `expiry`
    field, but keep the `access-token` field in the kubeconfig file so that
    the force-refresh works and continues using the access-token before
    server-side certificate-authority-data expires.
    
    b/179075455
    
    Change-Id: I8c41865b8ddfdbc01739168eea70f64b9094cb27

--- build/test-e2e-go/refresh-gcloud-creds.sh
+++ build/test-e2e-go/refresh-gcloud-creds.sh
@@ -5,13 +5,12 @@
 set -e
 
 while true; do
-  # Delete the lines containing `expiry: ` and `access-token: ` so that
+  # Delete the lines containing `expiry: ` so that
   # `gcloud config config-helper --force-auth-refresh` can force-update the `access_token`.
   # If expiry is defined, it only updates `access_token` when it expires even if `--force-auth-refresh` is provided.
   sed -i "/expiry: /d" "${HOME}/.kube/config"
-  sed -i "/access-token: /d" "${HOME}/.kube/config"
 
-  gcloud config config-helper --force-auth-refresh
+  gcloud --quiet config config-helper --force-auth-refresh
 
   # the gcloud credentials expire every 60 minutes, so force a refresh every 50 minutes.
   sleep 3000
