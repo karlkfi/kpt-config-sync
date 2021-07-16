6d2eeefddd2e1763d5a862d1915335785a9e853c
Jingfang Liu <jingfangliu@google.com>
2021-07-16 13:18:58 -0700

    Add e2e test for local-config annotation
    Bug: b/193744909
    Change-Id: I41389f342a90a629133f62f3fed8c6752c1beafe

---
+++ e2e/testcases/local_config_test.go
@@ -0,0 +1,116 @@
+package e2e
+
+import (
+	"testing"
+
+	"github.com/google/nomos/e2e/nomostest"
+	"github.com/google/nomos/pkg/core"
+	"github.com/google/nomos/pkg/metadata"
+	"github.com/google/nomos/pkg/syncer/syncertest"
+	"github.com/google/nomos/pkg/testing/fake"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestLocalConfig(t *testing.T) {
+	nt := nomostest.New(t)
+
+	ns := "local-config"
+	nt.Root.Add("acme/namespaces/local-config/ns.yaml",
+		fake.NamespaceObject(ns))
+
+	cmName := "e2e-test-configmap"
+	cmPath := "acme/namespaces/local-config/configmap.yaml"
+	cm := fake.ConfigMapObject(core.Name(cmName), core.Annotation(metadata.LocalConfigAnnotationKey, metadata.LocalConfigValue))
+	nt.Root.Add(cmPath, cm)
+	nt.Root.CommitAndPush("Adding ConfigMap as local config")
+	nt.WaitForRepoSyncs()
+
+	// Checking that the configmap doesn't exist in the cluster
+	err := nt.ValidateNotFound(cmName, ns, &corev1.ConfigMap{})
+	if err != nil {
+		nt.T.Fatal(err)
+	}
+
+	// Remove the local-config annotation
+	cm = fake.ConfigMapObject(core.Name(cmName))
+	nt.Root.Add(cmPath, cm)
+	nt.Root.CommitAndPush("Adding ConfigMap without local-config annotation")
+	nt.WaitForRepoSyncs()
+
+	// Checking that the configmap exist
+	err = nt.Validate(cmName, ns, &corev1.ConfigMap{})
+	if err != nil {
+		nt.T.Fatal(err)
+	}
+
+	// Add the local-config annotation again.
+	// This will make the object pruned.
+	cm = fake.ConfigMapObject(core.Name(cmName), core.Annotation(metadata.LocalConfigAnnotationKey, metadata.LocalConfigValue))
+	nt.Root.Add(cmPath, cm)
+	nt.Root.CommitAndPush("Changing ConfigMap to local config")
+	nt.WaitForRepoSyncs()
+
+	// Checking that the configmap is pruned.
+	err = nt.ValidateNotFound(cmName, ns, &corev1.ConfigMap{})
+	if err != nil {
+		nt.T.Fatal(err)
+	}
+}
+
+func TestLocalConfigWithManagementDisabled(t *testing.T) {
+	nt := nomostest.New(t)
+
+	ns := "local-config"
+	nt.Root.Add("acme/namespaces/local-config/ns.yaml",
+		fake.NamespaceObject(ns))
+
+	cmName := "e2e-test-configmap"
+	cmPath := "acme/namespaces/local-config/configmap.yaml"
+	cm := fake.ConfigMapObject(core.Name(cmName))
+	nt.Root.Add(cmPath, cm)
+	nt.Root.CommitAndPush("Adding ConfigMap")
+	nt.WaitForRepoSyncs()
+
+	// Checking that the configmap exist
+	err := nt.Validate(cmName, ns, &corev1.ConfigMap{})
+	if err != nil {
+		nt.T.Fatal(err)
+	}
+
+	// Add the management disabled annotation.
+	cm = fake.ConfigMapObject(core.Name(cmName), syncertest.ManagementDisabled)
+	nt.Root.Add(cmPath, cm)
+	nt.Root.CommitAndPush("Disable the management of ConfigMap")
+	nt.WaitForRepoSyncs()
+
+	// Checking that the configmap exist
+	err = nt.Validate(cmName, ns, &corev1.ConfigMap{})
+	if err != nil {
+		nt.T.Fatal(err)
+	}
+
+	// Add the local-config annotation to the unmanaged configmap
+	cm = fake.ConfigMapObject(core.Name(cmName), syncertest.ManagementDisabled,
+		core.Annotation(metadata.LocalConfigAnnotationKey, metadata.LocalConfigValue))
+	nt.Root.Add(cmPath, cm)
+	nt.Root.CommitAndPush("Change the ConfigMap to local config")
+	nt.WaitForRepoSyncs()
+
+	// Checking that the configmap exist
+	err = nt.Validate(cmName, ns, &corev1.ConfigMap{})
+	if err != nil {
+		nt.T.Fatal(err)
+	}
+
+	// Remove the management disabled annotation
+	cm = fake.ConfigMapObject(core.Name(cmName), core.Annotation(metadata.LocalConfigAnnotationKey, metadata.LocalConfigValue))
+	nt.Root.Add(cmPath, cm)
+	nt.Root.CommitAndPush("Remove the managed disabled annotation and keep the local-config annotation")
+	nt.WaitForRepoSyncs()
+
+	// Checking that the configmap exist
+	err = nt.Validate(cmName, ns, &corev1.ConfigMap{})
+	if err != nil {
+		nt.T.Fatal(err)
+	}
+}
