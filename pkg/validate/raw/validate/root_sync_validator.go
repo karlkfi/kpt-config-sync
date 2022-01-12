4dd847ea9cbc8c41f1f188a1c011ec3ff7904fb0
Nan Yu <nanyu@google.com>
2022-01-12 11:44:15 -0800

    Add a RootSync validator
    The source repository can include RootSync objects, so we need to
    validate the RootSync objects and convert to v1beta1 version if it is
    using v1alpha1.
    
    b/213343422
    
    Change-Id: Iabb81822c9c1cedd02deb0425c3fba2d9d2ba997

--- pkg/validate/raw/validate/root_sync_validator.go
+++ pkg/validate/raw/validate/root_sync_validator.go
@@ -9,36 +9,36 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-// RepoSync checks if the given FileObject is a RepoSync and if so, verifies
+// RootSync checks if the given FileObject is a RootSync and if so, verifies
 // that its fields are valid.
-func RepoSync(obj ast.FileObject) status.Error {
-	if obj.GetObjectKind().GroupVersionKind().GroupKind() != kinds.RepoSyncV1Beta1().GroupKind() {
+func RootSync(obj ast.FileObject) status.Error {
+	if obj.GetObjectKind().GroupVersionKind().GroupKind() != kinds.RootSyncV1Beta1().GroupKind() {
 		return nil
 	}
 	s, err := obj.Structured()
 	if err != nil {
 		return err
 	}
-	var rs *v1beta1.RepoSync
-	if obj.GroupVersionKind() == kinds.RepoSyncV1Alpha1() {
-		rs, err = toRepoSyncV1Beta1(s.(*v1alpha1.RepoSync))
+	var rs *v1beta1.RootSync
+	if obj.GroupVersionKind() == kinds.RootSyncV1Alpha1() {
+		rs, err = toRootSyncV1Beta1(s.(*v1alpha1.RootSync))
 		if err != nil {
 			return err
 		}
 	} else {
-		rs = s.(*v1beta1.RepoSync)
+		rs = s.(*v1beta1.RootSync)
 	}
 	return GitSpec(rs.Spec.Git, rs)
 }
 
-func toRepoSyncV1Beta1(rs *v1alpha1.RepoSync) (*v1beta1.RepoSync, status.Error) {
+func toRootSyncV1Beta1(rs *v1alpha1.RootSync) (*v1beta1.RootSync, status.Error) {
 	data, err := yaml.Marshal(rs)
 	if err != nil {
 		return nil, status.ResourceWrap(err, "failed marshalling", rs)
 	}
-	s := &v1beta1.RepoSync{}
+	s := &v1beta1.RootSync{}
 	if err := yaml.Unmarshal(data, s); err != nil {
-		return nil, status.ResourceWrap(err, "failed to convert to v1beta1 RepoSync", rs)
+		return nil, status.ResourceWrap(err, "failed to convert to v1beta1 RootSync", rs)
 	}
 	return s, nil
 }
