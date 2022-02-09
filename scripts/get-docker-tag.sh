4bc73859be5ab89ee9d9066794fd0efe895a15a3
Your <sdowell@google.com>
2022-02-09 19:10:51 +0000

    add license header to all files under scripts/
    This change is in preparation for releasing ConfigSync as an open source
    project. See go/releasing/preparing for more information. This change
    was produced by the addlicense tool.
    
    Bug: b/216203702
    Change-Id: I574add3127a3248560645f6c663332f56a1c58b8

--- scripts/get-docker-tag.sh
+++ scripts/get-docker-tag.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 # Helper script to get our docker tag if we're checked out on a branch
 # e.g. 1.4.0.  If we're on master, or if this is a local branch, and we use tag
